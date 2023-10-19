8d8dd919de1af77b55a46cbfa8d20c9fb7b6b31a
Allan Heremi <allanheremidev@gmail.com>
2023-10-19 10:31:55 +0200

    Style: style header signin button
    Co-authored-by: Rasmus Eklund <rasmus-eklund@users.noreply.github.com>
    Co-authored-by: rofunn <rofunn@users.noreply.github.com>

--- src/app/_components/HeaderComp.tsx
+++ src/app/_components/HeaderComp.tsx
@@ -2,15 +2,15 @@ import Image from "next/image";
 import Link from "next/link";
 import Login from "./Login";
 
-
 const Header = () => {
-  return(
-  <header className="flex items-center pl-4 md:pl-16 py-4 lg:py-8">
-    <Link href={"/"}>
-      <Image src="/logoBlack.png" alt="salt logo" width={130} height={30} />
-    </Link>
-    <Login />
-  </header>
-)};
+  return (
+    <header className="flex items-center justify-between px-4 py-4 md:px-12 lg:py-8 ">
+      <Link href={"/"}>
+        <Image src="/logoBlack.png" alt="salt logo" width={130} height={30} />
+      </Link>
+      <Login />
+    </header>
+  );
+};
 
 export default Header;
