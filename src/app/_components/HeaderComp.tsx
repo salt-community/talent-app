48cbba74053e2812c470e7c2afa7118b417e827f
Allan Heremi <allanheremidev@gmail.com>
2023-10-23 13:21:10 +0200

    Feat: Add developer only visible on sign in.
    Co-authored-by: rofunn <rofunn@users.noreply.github.com>
    Co-authored-by: Rasmus Eklund <rasmus-eklund@users.noreply.github.com>

--- src/app/_components/HeaderComp.tsx
+++ src/app/_components/HeaderComp.tsx
@@ -1,14 +1,19 @@
 import Image from "next/image";
 import Link from "next/link";
 import Login from "./Login";
+import { getServerAuthSession } from "@/server/auth";
 
-const Header = () => {
+
+const Header = async () => {
+  const session = await getServerAuthSession();
   return (
     <header className="flex items-center justify-between px-4 py-4 md:px-12 lg:py-8 ">
       <Link href={"/"}>
         <Image src="/logoBlack.png" alt="salt logo" width={130} height={30} />
       </Link>
+    { session &&
       <Link href={"/addDeveloper"}>Add developer</Link>
+    }  
       <Login />
     </header>
   );
