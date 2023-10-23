510503d2fb77966276ac5c056700b50af3667b0c
Rasmus Eklund <49008491+rasmus-eklund@users.noreply.github.com>
2023-10-23 12:01:24 +0200

    refactor: remove test form
    Co-authored-by: rofunn <rofunn@users.noreply.github.com>
    Co-authored-by: Allan Heremi <allanheremi@users.noreply.github.com>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,11 @@
 import { getServerAuthSession } from "@/server/auth";
 import SearchBar from "./_components/SearchBar";
-import EditDeveloperForm from "./_components/EditDeveloperForm";
 
 const Home = async () => {
   const session = await getServerAuthSession();
   return (
     <>
-      <div className="flex w-full flex-col items-center bg-gradient-to-b from-orange to-pink py-20 text-2xl font-bold text-white md:text-5xl">
+      <div className="flex w-full flex-col items-center bg-gradient-to-b from-orange to-pink py-20 text-xl font-bold text-white md:text-5xl">
         <div className="flex flex-col gap-2">
           <p>TOMORROW&apos;S DEVELOPERS.</p>
           <p>AVAILABLE.</p>
@@ -14,7 +13,8 @@ const Home = async () => {
         </div>
       </div>
       {session && <SearchBar />}
-      <EditDeveloperForm />
+
+      {/* <EditDeveloperForm /> */}
     </>
   );
 };
