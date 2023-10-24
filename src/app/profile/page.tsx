646cb67dd4acb2037809f5d34214ee48d2043ed5
Rasmus Eklund <49008491+rasmus-eklund@users.noreply.github.com>
2023-10-24 15:44:06 +0200

    feat: show projects

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,26 +1,39 @@
 import { api } from "@/trpc/server";
 import Developer from "../_components/profile/developer";
 import Project from "../_components/profile/project";
+import Container from "../_components/container";
+import Link from "next/link";
 
 const Profile = async () => {
-  const user = await api.developer.getByUserId.query();
-  const projects = await api.project.getByDev.query({developerId})
+  const developer = await api.developer.getByUser.query();
+  const projects = await api.project.getByDev.query();
 
   return (
-    <>
-      <h2 className="text-3xl">Your developers:</h2>
-      <ul>
-        {users.map((user) => (
-          <Developer user={user} />
-        ))}
-      </ul>
-      <h2 className="text-3xl">Your projects:</h2>
-      <ul>
-        {users.map((user) => (
-          <Project project={user} />
-        ))}
-      </ul>
-    </>
+    <div className="flex gap-2">
+      <div className="w-1/2">
+        <h2 className="text-3xl">Your developers:</h2>
+        <ul>
+          {developer ? (
+            <Developer developer={developer} />
+          ) : (
+            <Container>
+              <button>Create developer</button>
+            </Container>
+          )}
+        </ul>
+      </div>
+      <div className="w-1/2">
+        <h2 className="text-3xl">Your projects:</h2>
+        <ul className="flex flex-col gap-1">
+          {projects.map((project) => (
+            <Project key={project.id} project={project} />
+          ))}
+          <Container>
+            <Link href={"/profile/project"}>Create new project</Link>
+          </Container>
+        </ul>
+      </div>
+    </div>
   );
 };
 
