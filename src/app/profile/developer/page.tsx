2c80c218e528f01a6e19b80a360da93dd7b7d02f
Rasmus Eklund <49008491+rasmus-eklund@users.noreply.github.com>
2023-10-24 15:44:15 +0200

    refactor: move file

---
+++ src/app/profile/developer/page.tsx
@@ -0,0 +1,11 @@
+import AddDeveloperForm from "../../_components/AddDeveloper/AddDeveloperForm";
+
+const page = () => {
+  return (
+    <div>
+      <AddDeveloperForm />
+    </div>
+  );
+};
+
+export default page;
