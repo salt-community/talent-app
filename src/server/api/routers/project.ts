839c7415c8a38c590a3fbdb3d34c69adf41376c2
Rasmus Eklund <49008491+rasmus-eklund@users.noreply.github.com>
2023-10-24 15:41:13 +0200

    feat: get projects connected to developer, create project and connect project to dev

--- src/server/api/routers/project.ts
+++ src/server/api/routers/project.ts
@@ -1,23 +1,35 @@
+import { createTRPCRouter, protectedProcedure } from "@/server/api/trpc";
+import { projectSchema } from "@/utils/zodSchema";
 import { z } from "zod";
 
-import {
-  createTRPCRouter,
-  protectedProcedure,
-  publicProcedure,
-} from "@/server/api/trpc";
-import type { SearchResult } from "types";
-import { TRPCError } from "@trpc/server";
-
 export const projectRouter = createTRPCRouter({
-  getByDev: protectedProcedure
-    .input(z.object({ developerId: z.string().min(1) }))
-    .query(async ({ ctx, input: { developerId } }) => {
-      const projects = await ctx.db.project.findMany({
-        where: { Project_developer: { every: { developerId } } },
+  getByDev: protectedProcedure.query(async ({ ctx }) => {
+    const projects = await ctx.db.project_developer.findMany({
+      where: { Developer: { User: { some: { id: ctx.session.user.id } } } },
+      include: { project: true },
+    });
+
+    return projects.map(({ project }) => ({
+      id: project.id,
+      title: project.title,
+    }));
+  }),
+  create: protectedProcedure
+    .input(projectSchema)
+    .mutation(({ ctx, input: data }) => {
+      return ctx.db.project.create({ data });
+    }),
+
+  connectToDeveloper: protectedProcedure
+    .input(
+      z.object({
+        projectId: z.string().min(1),
+        developerId: z.string().min(1),
+      }),
+    )
+    .mutation(async ({ ctx, input: { developerId, projectId } }) => {
+      return await ctx.db.project_developer.create({
+        data: { projectId, developerId },
       });
-      return projects.map((project) => ({
-        id: project.id,
-        title: project.title,
-      }));
     }),
 });
