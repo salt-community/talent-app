73b120641e2ffd8b69258eed7f71d96f106a9bc0
Rasmus Eklund <49008491+rasmus-eklund@users.noreply.github.com>
2023-10-25 16:02:30 +0200

    feat: handle errors and load

--- src/app/profile/developer/[id]/page.tsx
+++ src/app/profile/developer/[id]/page.tsx
@@ -1,32 +1,54 @@
 "use client";
 import DeveloperForm from "@/app/_components/profile/developer/DeveloperForm";
 import { api } from "@/trpc/react";
-import type { tDevSchema } from "@/utils/zodSchema";
 import { useRouter } from "next/navigation";
 import toast from "react-hot-toast";
 
 type Props = { params: { id: string } };
 const DeveloperId = ({ params: { id } }: Props) => {
   const router = useRouter();
-  const { data: developer, isLoading, refetch } = api.developer.getById.useQuery({ id });
-  const { mutateAsync: updateDev } = api.developer.update.useMutation({
+  const {
+    data: developer,
+    isLoading,
+    isSuccess,
+    isError,
+    refetch,
+  } = api.developer.getById.useQuery({ id });
+  const { mutate: update } = api.developer.update.useMutation({
     onSuccess: async () => {
-      await refetch()
+      await refetch();
       router.push("/profile");
     },
     onError: (error) => {
       toast.error(error.message);
     },
   });
-  const handleData = async (data: tDevSchema) => {
-    await updateDev({ dev: data, id });
-  };
+  const { mutate: remove } = api.developer.delete.useMutation({
+    onSuccess: () => {
+      router.push("/profile");
+    },
+    onError: (error) => {
+      toast.error(error.message);
+    },
+  });
+
   return (
     <>
-      {isLoading ? (
-        <p>Loading...</p>
-      ) : (
-        <DeveloperForm handleData={handleData} developer={developer} />
+      {isLoading && <p>Loading...</p>}
+      {isError && <p>404</p>}
+      {isSuccess && (
+        <div className="flex flex-col gap-4">
+          <DeveloperForm
+            handleData={(dev) => update({ dev, id })}
+            developer={developer}
+          />
+          <button
+            className="rounded-lg bg-pink p-4 text-center"
+            onClick={() => remove(id)}
+          >
+            Delete
+          </button>
+        </div>
       )}
     </>
   );
