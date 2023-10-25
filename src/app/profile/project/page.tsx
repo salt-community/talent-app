bcda28a26e6192c15603988b79eb6b1f88be9fad
Rasmus Eklund <49008491+rasmus-eklund@users.noreply.github.com>
2023-10-25 14:36:21 +0200

    Feat: manage projects

--- src/app/profile/project/page.tsx
+++ src/app/profile/project/page.tsx
@@ -1,10 +1,45 @@
-import React from 'react'
+"use client";
+import JoinProject from "@/app/_components/profile/project/JoinProject";
+import ProjectForm from "@/app/_components/profile/project/ProjectForm";
+import { api } from "@/trpc/react";
+import { projectParams } from "@/utils/zodSchema";
+import { useRouter, useSearchParams } from "next/navigation";
+import React from "react";
+import toast from "react-hot-toast";
 
-const page = () => {
+const ProjectPage = () => {
+  const router = useRouter();
+  const { mutateAsync: join } = api.project.join.useMutation();
+  const { mutateAsync: create } = api.project.create.useMutation({
+    onSuccess: () => {
+      router.push("/profile");
+    },
+  });
+  const searchParams = useSearchParams();
+  const searchParamsObject = Object.fromEntries(searchParams);
+  const validatedSearchParams = projectParams.safeParse(searchParamsObject);
+  if (!validatedSearchParams.success) {
+    toast.error("Incorrect url");
+    router.push("/profile");
+    return;
+  }
+  const { id: developerId } = validatedSearchParams.data;
+  if (validatedSearchParams.data.do === "create") {
+    return (
+      <ProjectForm
+        handleData={async (project) => {
+          const { id: groupId } = await create(project);
+          await join({
+            developerId,
+            groupId,
+          });
+        }}
+      />
+    );
+  }
+  if (validatedSearchParams.data.do === "join") {
+    return <JoinProject developerId={developerId} />;
+  }
+};
 
-  return (
-    <div>page</div>
-  )
-}
-
-export default page
+export default ProjectPage;
