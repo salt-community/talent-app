10c920e5458388242b92c7bc81408b888b569976
Rasmus Eklund <49008491+rasmus-eklund@users.noreply.github.com>
2023-10-25 09:35:26 +0200

    Feat: data to render developer form

---
+++ src/app/_components/profile/developer/helpers/formPlaceholders.ts
@@ -0,0 +1,25 @@
+export const keys = [
+  "name",
+  "phone",
+  "mail",
+  "city",
+  "address",
+  "country",
+  "linkedinUrl",
+  "resume",
+  "title",
+  "description",
+] as const;
+
+export const placeholders = [
+  "First and last name",
+  "Phone number",
+  "E-mail address",
+  "City",
+  "Address",
+  "Country of residence",
+  "Link to LinkedIn",
+  "Link to Resume",
+  "Short description about yourself",
+  "E.g Full-stack Java developer",
+];
