0ccdddc42454df9d90977d6a0da1ebcb6852051d
Rasmus Eklund <49008491+rasmus-eklund@users.noreply.github.com>
2023-10-25 09:38:47 +0200

    feat: split zod schemas for developer

--- src/utils/zodSchema.ts
+++ src/utils/zodSchema.ts
@@ -2,20 +2,42 @@ import { z } from "zod";
 
 export const devSchema = z.object({
   name: z.string().min(2, "Provide a name"),
-  phone: z.string(),
+  phone: z.string().min(2),
   mail: z.string().email("Not valid mail"),
   city: z.string().min(2),
   address: z.string().min(2),
   country: z.string().min(2),
   linkedinUrl: z.string().url(),
   resume: z.string().url(),
-  title: z.string(),
+  title: z.string().min(2),
   description: z.string().min(2),
   skills: z.array(z.string()).min(1),
   gitHubUrl: z.string().url(),
   image: z.string().url(),
 });
 export type tDevSchema = z.infer<typeof devSchema>;
+
+export const devSchemaPartial = z.object({
+  name: z.string().min(2, "Provide a name"),
+  phone: z.string().min(2),
+  mail: z.string().email("Not valid mail"),
+  city: z.string().min(2),
+  address: z.string().min(2),
+  country: z.string().min(2),
+  linkedinUrl: z.string().url(),
+  resume: z.string().url(),
+  title: z.string().min(2),
+  description: z.string().min(2),
+});
+export type tDevSchemaPartial = z.infer<typeof devSchemaPartial>;
+
+export const githubSkillsSchema = z.object({
+  skills: z.array(z.string()).min(1),
+  gitHubUrl: z.string().url(),
+  image: z.string().url(),
+});
+
+export type tGithubSkillsSchema = z.infer<typeof githubSkillsSchema>;
 
 export const githubResponseSchema = z.object({
   avatar_url: z.string().url(),
