b25e71e4a152e6d5f25edaa0ceba02d4b4bfefcb
Rasmus Eklund <49008491+rasmus-eklund@users.noreply.github.com>
2023-10-25 14:36:47 +0200

    feat: edit project page

--- src/app/profile/project/[id]/page.tsx
+++ src/app/profile/project/[id]/page.tsx
@@ -1,12 +1,34 @@
+"use client";
 import ProjectForm from "@/app/_components/profile/project/ProjectForm";
-import { api } from "@/trpc/server";
+import { api } from "@/trpc/react";
 import React from "react";
 
 type Props = { params: { id: string } };
 
-const ProjectPage = async ({ params: { id } }: Props) => {
-  const projects = api.project.getByDev.query()
-  return <div><ProjectForm handleData={} project={}/></div>;
+const ProjectPage = ({ params: { id } }: Props) => {
+  const {
+    data: project,
+    isSuccess,
+    isLoading,
+    isError,
+  } = api.project.getById.useQuery({ id });
+  const { mutateAsync: updateProject } = api.project.update.useMutation();
+
+  return (
+    <div>
+      {isLoading && <p>Loading...</p>}
+      {isSuccess && (
+        <>
+          <h2 className="text-2xl">Edit project</h2>
+          <ProjectForm
+            handleData={(project) => updateProject({ id, project })}
+            project={project}
+          />
+        </>
+      )}
+      {isError && <p>404</p>}
+    </div>
+  );
 };
 
 export default ProjectPage;
