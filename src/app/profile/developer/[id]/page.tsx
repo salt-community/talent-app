3ef8c1233e0f10fb80fcaa0f5eb46b97f5c5bb40
Rasmus Eklund <49008491+rasmus-eklund@users.noreply.github.com>
2023-10-25 11:38:13 +0200

    feat: update developer

--- src/app/profile/developer/[id]/page.tsx
+++ src/app/profile/developer/[id]/page.tsx
@@ -1,23 +1,34 @@
 "use client";
 import DeveloperForm from "@/app/_components/profile/developer/DeveloperForm";
 import { api } from "@/trpc/react";
-import type { RouterInputs } from "@/trpc/shared";
-type Developer = RouterInputs["developer"]["create"];
+import type { tDevSchema } from "@/utils/zodSchema";
+import { useRouter } from "next/navigation";
+import toast from "react-hot-toast";
 
 type Props = { params: { id: string } };
 const DeveloperId = ({ params: { id } }: Props) => {
-  const { data: developer, isLoading } = api.developer.getById.useQuery({ id });
-  const handleData = async (data: Developer) => {
-    console.log(data);
+  const router = useRouter();
+  const { data: developer, isLoading, refetch } = api.developer.getById.useQuery({ id });
+  const { mutateAsync: updateDev } = api.developer.update.useMutation({
+    onSuccess: async () => {
+      await refetch()
+      router.push("/profile");
+    },
+    onError: (error) => {
+      toast.error(error.message);
+    },
+  });
+  const handleData = async (data: tDevSchema) => {
+    await updateDev({ dev: data, id });
   };
   return (
-    <div>
+    <>
       {isLoading ? (
         <p>Loading...</p>
       ) : (
         <DeveloperForm handleData={handleData} developer={developer} />
       )}
-    </div>
+    </>
   );
 };
 
