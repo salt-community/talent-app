fc884f9239225b452ce1a12e8683d502b86b3132
Rasmus Eklund <49008491+rasmus-eklund@users.noreply.github.com>
2023-10-26 12:09:13 +0200

    Style
    Co-authored-by: rofunn <rofunn@users.noreply.github.com>
    Co-authored-by: Allan Heremi <allanheremi@users.noreply.github.com>

--- src/app/_components/FormError.tsx
+++ src/app/_components/FormError.tsx
@@ -1,7 +1,7 @@
 import type { FieldError } from "react-hook-form";
 
 const FormError = ({ error }: { error: FieldError | undefined }) => {
-  if (error?.message) return <p className="bg-orange px-1 text-sm">{error.message}</p>;
+  if (error?.message) return <p className="bg-orange px-1 text-sm rounded-md animate-pulse">{error.message}</p>;
 };
 
 export default FormError;
