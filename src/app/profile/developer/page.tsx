da624166e66270b353ec69b434ddea7f56aed2ff
Rasmus Eklund <49008491+rasmus-eklund@users.noreply.github.com>
2023-10-26 15:45:57 +0200

    Feat: disable buttons until success

--- src/app/profile/developer/page.tsx
+++ src/app/profile/developer/page.tsx
@@ -5,27 +5,36 @@ import { useRouter } from "next/navigation";
 import toast from "react-hot-toast";
 import { useSession } from "next-auth/react";
 import defaultDeveloperData from "@/app/_components/profile/developer/helpers/defaultDeveloperData";
+import Button from "@/app/_components/Button";
 
 const DeveloperPage = () => {
   const { data } = useSession();
   const router = useRouter();
-  const { mutate: createUser } = api.developer.create.useMutation({
-    onSuccess: () => {
-      router.push("/profile");
-    },
-    onError: (error) => {
-      toast.error(error.message);
-    },
-  });
+  const { mutate: createUser, isLoading: creatingUser } =
+    api.developer.create.useMutation({
+      onSuccess: () => {
+        router.push("/profile");
+      },
+      onError: (error) => {
+        toast.error(error.message);
+      },
+    });
   const newData = defaultDeveloperData();
   if (data) {
     newData.name = data.user.name ?? "";
     newData.mail = data.user.email ?? "";
   }
   return (
-    <div className="max-w-md">
-      <DeveloperForm developer={newData} handleData={createUser} />
-    </div>
+    <>
+      <div className="relative max-w-md">
+        <DeveloperForm developer={newData} handleData={createUser}>
+          <Button className="py-2" disabled={creatingUser} form="developer-form">
+            Create user
+          </Button>
+        </DeveloperForm>
+      </div>
+      <Button className="absolute bottom-2 right-2 py-2">Go back</Button>
+    </>
   );
 };
 
