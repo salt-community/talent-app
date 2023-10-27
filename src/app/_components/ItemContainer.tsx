0428c404f059c7da04ebefa01b396c9398de7bf9
Allan Heremi <allanheremidev@gmail.com>
2023-10-27 15:58:53 +0200

    Style: animation durartion
    Co-authored-by: Rasmus Eklund <rasmus-eklund@users.noreply.github.com>

--- src/app/_components/ItemContainer.tsx
+++ src/app/_components/ItemContainer.tsx
@@ -5,7 +5,7 @@ type Props = { children: React.ReactNode; className?: string };
 const ItemContainer = ({ children, className }: Props) => {
   return (
     <li
-      className={`flex items-center gap-2 rounded-md border-2 border-black/30 p-2 shadow-lg duration-500 ease-linear hover:bg-orange/50 md:p-4 ${className}`}
+      className={`flex items-center gap-2 rounded-md border-2 border-black/30 p-2 shadow-lg duration-300 ease-linear hover:bg-orange/50 md:p-4 ${className}`}
     >
       {children}
     </li>
