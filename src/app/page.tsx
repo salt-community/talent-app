b38f05c9a561880a0fbd5afa5a4426e5c0ed0691
Rasmus Eklund <49008491+rasmus-eklund@users.noreply.github.com>
2023-10-27 10:50:39 +0200

    feat: shrink and animate
    Co-authored-by: rofunn <rofunn@users.noreply.github.com>
    Co-authored-by: Allan Heremi <allanheremi@users.noreply.github.com>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,11 @@ const Home = () => {
 
   return (
     <>
-      <div className="flex w-full flex-col items-center bg-gradient-to-b from-orange to-pink py-20 text-xl font-bold text-white md:text-5xl">
+      <div
+        className={`flex w-full flex-col items-center bg-gradient-to-b from-orange to-pink duration-500 ease-linear ${
+          search ? "py-2" : "py-20"
+        } text-xl font-bold text-white md:text-5xl`}
+      >
         <div className="flex flex-col gap-2">
           <p>TOMORROW&apos;S DEVELOPERS.</p>
           <p>AVAILABLE.</p>
