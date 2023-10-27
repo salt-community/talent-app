f5c4b4fb06ed8614bdfe1530a9fec1b5ee1c3442
Rasmus Eklund <49008491+rasmus-eklund@users.noreply.github.com>
2023-10-27 06:20:38 +0200

    Style: more responsive

--- src/app/_components/search/SearchItem.tsx
+++ src/app/_components/search/SearchItem.tsx
@@ -1,4 +1,3 @@
-import type { FC } from "react";
 import Image from "next/image";
 import Link from "next/link";
 import Github from "../icons/Github";
@@ -7,43 +6,48 @@ import type { RouterOutputs } from "@/trpc/shared";
 import ItemContainer from "../ItemContainer";
 
 type DeveloperSearch = RouterOutputs["developer"]["getBySearch"][number];
-type DeveloperListProps = {
-  consultant: DeveloperSearch;
+type Props = {
+  developer: DeveloperSearch;
 };
 
-const SearchItem: FC<DeveloperListProps> = ({ consultant }) => {
+const SearchItem = ({ developer }: Props) => {
   return (
     <ItemContainer className="justify-between">
       <Link
-        href={`developer/${consultant.id}`}
+        href={`developer/${developer.id}`}
         className="flex items-center gap-4"
       >
         <Image
-          src={consultant.image}
+          src={developer.image}
           alt="Image"
           width={48}
           height={48}
           className="rounded-full border-2 border-black"
         />
-        <h2 className="text-sm font-bold md:text-2xl">{consultant.name}</h2>
-        <p className="hidden md:block">{consultant.title}</p>
+        <h2 className="shrink-0 grow text-sm font-bold md:text-2xl">
+          {developer.name}
+        </h2>
+        <p className="hidden whitespace-nowrap overflow-hidden overflow-ellipsis md:block">
+          {developer.title}
+        </p>
       </Link>
-      <ul className="hidden gap-4 lg:flex">
-        {consultant.skills.slice(0, 4).map((skill, index) => (
-          <li key={skill + index}>
-            <p className="rounded-full bg-orange px-4 py-1 text-sm text-white">
-              {skill}
-            </p>
+      <ul className="hidden grow gap-4 lg:flex">
+        {developer.skills.slice(0, 4).map((skill, index) => (
+          <li
+            className="rounded-full bg-orange px-4 py-1 text-sm text-white shrink-0"
+            key={skill + index}
+          >
+            <p>{skill}</p>
           </li>
         ))}
       </ul>
-      <div className="flex items-center ">
+      <div className="flex items-center">
         <Github
-          url={consultant.gitHubUrl}
+          url={developer.gitHubUrl}
           className={"h-10 w-10 md:h-16 md:w-16"}
         />
         <LinkedIn
-          url={consultant.linkedinUrl}
+          url={developer.linkedinUrl}
           className={"h-10 w-10 md:h-16 md:w-16"}
         />
       </div>
