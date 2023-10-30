0da8eaee72eee88634279729981e9e78091d60a7
Rasmus Eklund <49008491+rasmus-eklund@users.noreply.github.com>
2023-10-30 14:31:24 +0100

    Refactor: move div inside of component
    Co-authored-by: Allan Heremi <allanheremi@users.noreply.github.com>

--- src/app/developer/[id]/page.tsx
+++ src/app/developer/[id]/page.tsx
@@ -1,10 +1,10 @@
 import Link from "next/link";
 import ContactCard from "@/app/_components/developer/DeveloperContactCard";
 import SectionHeader from "@/app/_components/SectionHeader";
-import Projects from "@/app/_components/developer/DeveloperProjects";
 import Skills from "@/app/_components/developer/DeveloperSkills";
 import TeamMembers from "@/app/_components/developer/DeveloperTeamMembers";
 import { api } from "@/trpc/server";
+import ProjectSlider from "@/app/_components/developer/ProjectSlider";
 
 const DeveloperPage = async ({
   params: { id },
@@ -15,21 +15,20 @@ const DeveloperPage = async ({
   return (
     <div className="flex grow justify-center bg-gradient-to-b from-orange to-pink px-10 ">
       <div className="flex flex-col gap-4 py-6 md:w-[95%] md:flex-row md:py-0">
-        <div className="rounded-md bg-gray md:w-1/4 md:rounded-none">
-          <ContactCard developer={developer} />
-        </div>
+        <ContactCard developer={developer} />
         <div className="flex flex-col gap-12 rounded-md bg-gray p-4 text-xl md:w-3/4 md:rounded-none md:px-10">
           <section className="flex flex-col gap-4">
             <SectionHeader title={developer.title} />
             <p>{developer.description}</p>
           </section>
-          <Projects projects={developer.projects} />
+          <ProjectSlider projects={developer.projects} />
           <Skills skills={developer.skills} />
           {developer.mobs.map(
             (mob) => mob && <TeamMembers key={mob.id} mob={mob} />,
           )}
         </div>
       </div>
+
       <Link
         href={"/"}
         className="fixed bottom-5 right-5 rounded-md border-2 border-black bg-white p-2 text-black hover:bg-orange hover:text-white"
