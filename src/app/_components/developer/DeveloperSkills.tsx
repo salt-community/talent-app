7c9efc7ffc2f4d3c9d53857f9305fac68c22277c
Rasmus Eklund <49008491+rasmus-eklund@users.noreply.github.com>
2023-10-30 14:27:35 +0100

    Feat: expandable skills
    Co-authored-by: Allan Heremi <allanheremi@users.noreply.github.com>

--- src/app/_components/developer/DeveloperSkills.tsx
+++ src/app/_components/developer/DeveloperSkills.tsx
@@ -1,18 +1,31 @@
-import type { FC } from "react";
+"use client";
+import { useState } from "react";
 import SectionHeader from "../SectionHeader";
 
 type Props = { skills: string[] };
 
-const Skills: FC<Props> = ({ skills }) => {
+const Skills = ({ skills }: Props) => {
+  const [more, setMore] = useState(false);
   return (
     <div className="flex flex-col gap-4">
-      <SectionHeader title="Skills"/>
+      <SectionHeader title="Skills" />
       <ul className="flex flex-wrap gap-4">
-        {skills.map((skill, index) => (
-          <li key={skill + index}>
-            <p className="bg-orange px-4 py-1 rounded-full text-white">{skill}</p>
-          </li>
-        ))}
+        {skills
+          .filter((_, i) => more || i < 4)
+          .map((skill, index) => {
+            return (
+              <li key={skill + index}>
+                <p className="rounded-full bg-orange px-4 py-1 text-sm text-white">
+                  {skill}
+                </p>
+              </li>
+            );
+          })}
+        <li>
+          <button className="text-sm underline underline-offset-2" onClick={() => setMore((p) => !p)}>
+            {more ? "Show less" : "Show more"}
+          </button>
+        </li>
       </ul>
     </div>
   );
