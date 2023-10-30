6de8411f6cd760dfdefda5cf29ef382c4c0de533
Rasmus Eklund <49008491+rasmus-eklund@users.noreply.github.com>
2023-10-30 16:55:58 +0100

    Style: show 4 skills + number
    Co-authored-by: Allan Heremi <allanheremi@users.noreply.github.com>

--- src/app/_components/search/SearchItem.tsx
+++ src/app/_components/search/SearchItem.tsx
@@ -11,32 +11,37 @@ type Props = {
 const SearchItem = ({ developer }: Props) => {
   return (
     <Link href={`developer/${developer.id}`}>
-      <ItemContainer className="justify-between">
-        <div className="flex items-center gap-2">
-          <Image
-            src={developer.image}
-            alt="Image"
-            width={48}
-            height={48}
-            className="rounded-full border-2 border-black"
-          />
-          <h2 className="shrink-0 grow text-sm font-bold md:text-2xl">
-            {developer.name}
-          </h2>
-          <p className="overflow-hidden overflow-ellipsis whitespace-nowrap text-xs">
-            {developer.title}
-          </p>
+      <ItemContainer>
+        <Image
+          src={developer.image}
+          alt="Image"
+          width={48}
+          height={48}
+          className="rounded-full border-2 border-black"
+        />
+        <div className="flex grow flex-col">
+          <h2 className="text-sm font-bold md:text-2xl">{developer.name}</h2>
+          <div className="flex justify-between">
+            <ul className="flex gap-1 md:gap-2">
+              {developer.skills.slice(0, 4).map((skill, index) => (
+                <li
+                  className="flex items-center rounded-sm bg-orange px-1 text-[0.6rem] text-white md:rounded-full md:p-1.5"
+                  key={skill + index}
+                >
+                  <p className="whitespace-nowrap">{skill}</p>
+                </li>
+              ))}
+            </ul>
+            {developer.skills.length - 4 > 0 && (
+              <p className="flex items-center rounded-full bg-orange px-1 text-[0.6rem] text-white md:hidden">
+                +{developer.skills.length - 4}
+              </p>
+            )}
+          </div>
         </div>
-        <ul className="hidden gap-4 lg:flex">
-          {developer.skills.slice(0, 4).map((skill, index) => (
-            <li
-              className="shrink-0 rounded-full bg-orange px-4 py-1 text-sm text-white"
-              key={skill + index}
-            >
-              <p>{skill}</p>
-            </li>
-          ))}
-        </ul>
+        <p className="hidden overflow-hidden overflow-ellipsis whitespace-nowrap text-xs md:flex">
+          {developer.title}
+        </p>
       </ItemContainer>
     </Link>
   );
