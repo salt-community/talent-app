e61b0d534f262d1f160840b85e7b16386a32709c
Rasmus Eklund <49008491+rasmus-eklund@users.noreply.github.com>
2023-10-30 16:54:13 +0100

    Style: reorganize elements
    Co-authored-by: Allan Heremi <allanheremi@users.noreply.github.com>

--- src/app/_components/developer/DeveloperContactCard.tsx
+++ src/app/_components/developer/DeveloperContactCard.tsx
@@ -1,10 +1,11 @@
-import type { FC } from "react";
+"use client";
 import Image from "next/image";
 import Github from "../../assets/icons/Github";
 import { mdilEmail, mdilPhone, mdilMapMarker } from "@mdi/light-js";
 import IconTemp from "../../assets/icons/IconTemp";
 import LinkedIn from "../../assets/icons/LinkedIn";
 import type { RouterOutputs } from "@/trpc/shared";
+import { useState } from "react";
 
 type Developer = RouterOutputs["developer"]["getById"];
 
@@ -12,45 +13,54 @@ type ContactCardProps = {
   developer: Developer;
 };
 
-const ContactCard: FC<ContactCardProps> = ({ developer }) => {
+const ContactCard = ({ developer }: ContactCardProps) => {
+  const [open, setOpen] = useState(false);
   return (
-    <section className="flex flex-col gap-6 p-4 pt-8">
-      <div className="flex flex-col items-center gap-2">
-        <Image
-          className="rounded-full "
-          src={developer.image}
-          alt="profile picture"
-          width={100}
-          height={100}
-        />
-        <h1 className="text-center text-2xl md:text-4xl">{developer.name}</h1>
-      </div>
-      <div className="flex flex-col gap-4 p-2">
-        <div className="flex gap-1">
-          <IconTemp path={mdilPhone} />
-          <p>{developer.phone}</p>
-        </div>
-        <div className="flex items-center gap-1">
-          <IconTemp path={mdilEmail} />
-          <a
-            className="text-orange underline"
-            href={`mailto: ${developer.mail}`}
-          >
-            email me
-          </a>
-        </div>
-        <div className="flex items-center gap-1">
-          <IconTemp path={mdilMapMarker} />
-          <div className="flex flex-wrap gap-1">
-            <p>{developer.address}</p>
-            <p>{developer.city}</p>
-            <p>{developer.country}</p>
+    <section className="rounded-md bg-gray md:w-1/4 md:rounded-none">
+      <div className="flex flex-col gap-2 p-2">
+        <div className="flex flex-col items-center self-center">
+          <Image
+            className="h-28 w-28 rounded-full"
+            src={developer.image}
+            alt="profile picture"
+            width={100}
+            height={100}
+          />
+          <h1 className="font-semibold">{developer.name}</h1>
+          <div className="flex gap-2">
+            <Github url={developer.gitHubUrl} className={"h-8 w-8"} />
+            <LinkedIn url={developer.linkedinUrl} className={"h-8 w-8"} />
           </div>
         </div>
-      </div>
-      <div className="flex">
-        <Github url={developer.gitHubUrl} className={"h-10 w-10"} />
-        <LinkedIn url={developer.linkedinUrl} className={"h-10 w-10"} />
+
+        {open && (
+          <div className="flex flex-col gap-2 text-sm">
+            <div className="flex gap-1">
+              <IconTemp path={mdilPhone} />
+              <p>{developer.phone}</p>
+            </div>
+            <div className="flex items-center gap-1">
+              <IconTemp path={mdilEmail} />
+              <a
+                className="text-orange underline"
+                href={`mailto: ${developer.mail}`}
+              >
+                email me
+              </a>
+            </div>
+            <div className="flex items-center gap-1">
+              <IconTemp path={mdilMapMarker} />
+              <div className="flex gap-1">
+                <p>{developer.address}</p>
+                <p>{developer.city},</p>
+                <p>{developer.country}</p>
+              </div>
+            </div>
+          </div>
+        )}
+        <button className="self-end" onClick={() => setOpen((p) => !p)}>
+          {open ? "Close" : "Contact"}
+        </button>
       </div>
     </section>
   );
