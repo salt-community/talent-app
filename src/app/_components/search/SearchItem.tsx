1c01c5296a3aac18c92f7de4531de03b4658432c
Rasmus Eklund <49008491+rasmus-eklund@users.noreply.github.com>
2023-10-30 10:31:22 +0100

    Style: make whole card clickable
    Co-authored-by: Allan Heremi <allanheremi@users.noreply.github.com>

--- src/app/_components/search/SearchItem.tsx
+++ src/app/_components/search/SearchItem.tsx
@@ -12,46 +12,45 @@ type Props = {
 
 const SearchItem = ({ developer }: Props) => {
   return (
-    <ItemContainer className="justify-between">
-      <Link
-        href={`developer/${developer.id}`}
-        className="flex items-center gap-4"
-      >
-        <Image
-          src={developer.image}
-          alt="Image"
-          width={48}
-          height={48}
-          className="rounded-full border-2 border-black"
-        />
-        <h2 className="shrink-0 grow text-sm font-bold md:text-2xl">
-          {developer.name}
-        </h2>
-        <p className="hidden whitespace-nowrap overflow-hidden overflow-ellipsis md:block">
+    <Link href={`developer/${developer.id}`}>
+      <ItemContainer className="justify-between">
+        <div className="flex items-center gap-4">
+          <Image
+            src={developer.image}
+            alt="Image"
+            width={48}
+            height={48}
+            className="rounded-full border-2 border-black"
+          />
+          <h2 className="shrink-0 grow text-sm font-bold md:text-2xl">
+            {developer.name}
+          </h2>
+        </div>
+        <p className="hidden overflow-hidden overflow-ellipsis whitespace-nowrap md:block">
           {developer.title}
         </p>
-      </Link>
-      <ul className="hidden grow gap-4 lg:flex">
-        {developer.skills.slice(0, 4).map((skill, index) => (
-          <li
-            className="rounded-full bg-orange px-4 py-1 text-sm text-white shrink-0"
-            key={skill + index}
-          >
-            <p>{skill}</p>
-          </li>
-        ))}
-      </ul>
-      <div className="flex items-center">
-        <Github
-          url={developer.gitHubUrl}
-          className={"h-10 w-10 md:h-16 md:w-16"}
-        />
-        <LinkedIn
-          url={developer.linkedinUrl}
-          className={"h-10 w-10 md:h-16 md:w-16"}
-        />
-      </div>
-    </ItemContainer>
+        <ul className="hidden grow gap-4 lg:flex">
+          {developer.skills.slice(0, 4).map((skill, index) => (
+            <li
+              className="shrink-0 rounded-full bg-orange px-4 py-1 text-sm text-white"
+              key={skill + index}
+            >
+              <p>{skill}</p>
+            </li>
+          ))}
+        </ul>
+        <div className="flex items-center">
+          <Github
+            url={developer.gitHubUrl}
+            className={"h-10 w-10 md:h-16 md:w-16"}
+          />
+          <LinkedIn
+            url={developer.linkedinUrl}
+            className={"h-10 w-10 md:h-16 md:w-16"}
+          />
+        </div>
+      </ItemContainer>
+    </Link>
   );
 };
 
