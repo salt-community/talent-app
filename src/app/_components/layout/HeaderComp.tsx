7d64086e96f08a19c3ab005a2974f99e907358f6
Rasmus Eklund <49008491+rasmus-eklund@users.noreply.github.com>
2023-11-07 17:04:43 +0100

    Style: make header smaller

--- src/app/_components/layout/HeaderComp.tsx
+++ src/app/_components/layout/HeaderComp.tsx
@@ -6,8 +6,10 @@ import { useSession } from "next-auth/react";
 
 const Header = () => {
   const session = useSession();
+  const className =
+    "hover:underline font-semibold duration-200 bg-orange/20 active:bg-orange px-2 py-1 rounded-md";
   return (
-    <header className="flex items-center justify-between px-4 py-4 md:px-12 lg:py-8 ">
+    <header className="flex items-center justify-between px-5 py-3 md:px-12 lg:py-8 ">
       <Link href={"/"}>
         <Image
           className="w-20 md:w-auto"
@@ -17,17 +19,13 @@ const Header = () => {
           height={30}
         />
       </Link>
-
       <div className="flex gap-2">
         {session.status === "authenticated" && (
-          <Link
-            className="rounded-md bg-orange/20 p-2 font-semibold duration-200 hover:underline active:bg-orange"
-            href={"/profile"}
-          >
+          <Link className={className} href={"/profile"}>
             Profile
           </Link>
         )}
-        <Login />
+        <Login className={className} />
       </div>
     </header>
   );
