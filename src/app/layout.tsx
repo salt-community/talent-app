59576697cb178ba61be879bd0b0f7312378da8ce
Rasmus Eklund <49008491+rasmus-eklund@users.noreply.github.com>
2023-11-07 16:13:14 +0100

    Style: remove sticky header

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -30,8 +30,8 @@ export default function RootLayout({
         <TRPCReactProvider headers={headers()}>
           <Toaster />
           <SessionProviderWrapper>
-            <Header />
             <main className="flex grow flex-col overflow-y-auto">
+              <Header />
               {children}
             </main>
           </SessionProviderWrapper>
