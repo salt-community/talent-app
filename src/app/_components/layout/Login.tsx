464f593fe13cd39d7b7e257438c76528673133ac
Rasmus Eklund <49008491+rasmus-eklund@users.noreply.github.com>
2023-11-09 07:36:02 +0100

    refactor: less code

--- src/app/_components/layout/Login.tsx
+++ src/app/_components/layout/Login.tsx
@@ -1,32 +1,22 @@
 "use client";
 import { signIn, signOut, useSession } from "next-auth/react";
 import { useRouter } from "next/navigation";
-import toast from "react-hot-toast";
 
-const Login = ({className}:{className:string}) => {
+const Login = ({ className }: { className: string }) => {
   const { data: session } = useSession();
   const router = useRouter();
-  const handleLogin = () => {
-    signIn().catch(() =>
-      toast.error("Could not log in."),
-    );
-  };
-  const handleLogout = () => {
-    signOut()
-      .then(() => {
-        router.push("/");
-      })
-      .catch(() => toast.error("Could not log out."));
-  };
   return (
     <>
       {!session && (
-        <button className={className} onClick={handleLogin}>
+        <button className={className} onClick={() => void signIn()}>
           Sign In
         </button>
       )}
       {session && (
-        <button className={className} onClick={handleLogout}>
+        <button
+          className={className}
+          onClick={() => void signOut().then(() => router.push("/"))}
+        >
           Sign Out
         </button>
       )}
