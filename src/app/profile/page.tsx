49efc80614a18b5aeaeb4c3199145ba105209818
Rasmus Eklund <49008491+rasmus-eklund@users.noreply.github.com>
2023-11-09 14:31:55 +0100

    fix: remove blocking profile

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -4,11 +4,8 @@ import Project from "../_components/profile/ProjectItem";
 import Link from "next/link";
 import ItemContainer from "../_components/ItemContainer";
 import MobItem from "../_components/profile/MobItem";
-import checkIfAuth from "@/utils/redirectIfNotAuth";
-import { getServerAuthSession } from "@/server/auth";
 
 const Profile = async () => {
-  checkIfAuth(await getServerAuthSession());
   const developer = await api.developer.getByUser.query();
   const projects = await api.project.getByDev.query();
   const mobs = await api.mob.getByDev.query();
--- src/utils/redirectIfNotAuth.ts
+++ src/utils/redirectIfNotAuth.ts
@@ -2,7 +2,7 @@ import type { Session } from "next-auth";
 import { redirect } from "next/navigation";
 
 const checkIfAuth = (session: Session | null) => {
-  if (!!!session || !!!session.user) {
+  if (!session) {
     redirect("/api/auth/signin");
   }
 };
