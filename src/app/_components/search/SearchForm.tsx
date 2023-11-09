9673ca07a0cd8d40b05fc01135b2a3f342d08bc0
Rasmus Eklund <49008491+rasmus-eklund@users.noreply.github.com>
2023-11-09 14:32:05 +0100

    feat: search icon

--- src/app/_components/search/SearchForm.tsx
+++ src/app/_components/search/SearchForm.tsx
@@ -1,9 +1,9 @@
 "use client";
-import Button from "../Button";
 import { type SubmitHandler, useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { searchDevSchema, type tSearchDevSchema } from "@/utils/zodSchema";
 import FormError from "../FormError";
+import Icon from "@/app/assets/icons/Icon";
 
 type Props = { onSearch: (search: string) => void };
 const SearchForm = ({ onSearch }: Props) => {
@@ -18,18 +18,21 @@ const SearchForm = ({ onSearch }: Props) => {
   };
   return (
     <form
-      className="flex gap-2 grow md:w-3/5"
+      className="flex grow gap-2 rounded-md border-2 border-black/50 bg-white md:w-3/5"
       onSubmit={(event) => void handleSubmit(onSubmit)(event)}
     >
       <input
-        className="min-w-0 w-3/4 rounded-md border-2 border-black/50 px-2"
+        className="min-w-0 grow px-2 outline-none"
         {...register("search")}
         placeholder="Search..."
       />
       <FormError error={errors.search} />
-      <Button className="px-2 py-2 w-1/4" inverted>
-        Search
-      </Button>
+      <button type="submit">
+        <Icon
+          className="h-8 w-8 fill-black/30 md:hover:fill-black"
+          icon="search"
+        />
+      </button>
     </form>
   );
 };
