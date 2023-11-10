b5e403d051112b622df410b5190566f15ed25a2a
Rasmus Eklund <49008491+rasmus-eklund@users.noreply.github.com>
2023-11-10 17:15:24 +0100

    Style: padding

--- src/app/_components/developer/Projects.tsx
+++ src/app/_components/developer/Projects.tsx
@@ -8,29 +8,29 @@ type Props = {
 };
 const Projects = ({ projects }: Props) => {
   return (
-    <section className="flex flex-col gap-2">
-      <ul>
+    <section>
+      <ul className="flex flex-col gap-4">
         {projects.map((project) => (
           <div key={project.id} className="flex flex-col gap-4">
-          <div className="flex items-center gap-2">
-            <SectionHeader title={project.title} />{" "}
-            <a className="w-8" href={project.githublink} target="_blank">
-              <Icon icon="github" className="h-8 w-8 fill-black" />
-            </a>
+            <div className="flex items-center gap-2">
+              <SectionHeader title={project.title} />{" "}
+              <a className="w-8" href={project.githublink} target="_blank">
+                <Icon icon="github" className="h-8 w-8 fill-black" />
+              </a>
+            </div>
+            <div className="relative pb-[56.25%]">
+              <iframe
+                className="absolute left-0 top-0 h-full w-full"
+                src={`https://www.youtube.com/embed/${project.youtube}`}
+                title={project.title}
+                width={"100%"}
+                height={"100%"}
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                allowFullScreen
+              ></iframe>
+            </div>
+            <p>{project.description}</p>
           </div>
-          <div className="relative pb-[56.25%]">
-            <iframe
-              className="absolute left-0 top-0 h-full w-full"
-              src={`https://www.youtube.com/embed/${project.youtube}`}
-              title={project.title}
-              width={"100%"}
-              height={"100%"}
-              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-              allowFullScreen
-            ></iframe>
-          </div>
-          <p>{project.description}</p>
-        </div>
         ))}
       </ul>
     </section>
