94eb0545dcbbcd9d59c2839d061100b1c5d9256c
Rasmus Eklund <49008491+rasmus-eklund@users.noreply.github.com>
2023-11-10 17:15:44 +0100

    Refactor: remove unused conditional

--- src/app/_components/developer/Team.tsx
+++ src/app/_components/developer/Team.tsx
@@ -12,31 +12,32 @@ type Props = {
 const TeamMembers = ({ mobs }: Props) => {
   return (
     <>
-      {!!mobs &&
-        mobs.map((mob) => (
-          <section className="flex w-full flex-col gap-4" key={mob.id}>
-            <SectionHeader title="Team members" />
-            <ul className="flex flex-wrap justify-evenly gap-1">
-              {mob.members.map((developer) => (
-                <li key={developer.id}>
-                  <Link
-                    className="flex flex-col items-center rounded-md px-2 duration-300 ease-in-out hover:translate-y-[-2%] hover:bg-orange/20 active:bg-orange"
-                    href={`/developer/${developer.id}`}
-                  >
-                    <Image
-                      src={developer.image}
-                      alt="image"
-                      width={50}
-                      height={50}
-                      className="h-12 w-12 rounded-full"
-                    />
-                    <p className="text-center text-sm whitespace-nowrap">{developer.name}</p>
-                  </Link>
-                </li>
-              ))}
-            </ul>
-          </section>
-        ))}
+      {mobs.map((mob) => (
+        <section className="flex w-full flex-col gap-4" key={mob.id}>
+          <SectionHeader title="Team members" />
+          <ul className="flex flex-wrap justify-evenly gap-1">
+            {mob.members.map((developer) => (
+              <li key={developer.id}>
+                <Link
+                  className="flex flex-col items-center rounded-md px-2 duration-300 ease-in-out hover:translate-y-[-2%] hover:bg-orange/20 active:bg-orange"
+                  href={`/developer/${developer.id}`}
+                >
+                  <Image
+                    src={developer.image}
+                    alt="image"
+                    width={50}
+                    height={50}
+                    className="h-12 w-12 rounded-full"
+                  />
+                  <p className="whitespace-nowrap text-center text-sm">
+                    {developer.name}
+                  </p>
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </section>
+      ))}
     </>
   );
 };
