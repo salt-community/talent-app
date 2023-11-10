45950d14708569bd52a44de00000f81171b019ad
makoalex <macovei.a2@gmail.com>
2023-11-10 15:17:41 +0100

    style: styles added to  Item component contents

--- src/app/_components/search/SearchItem.tsx
+++ src/app/_components/search/SearchItem.tsx
@@ -20,19 +20,19 @@ const SearchItem = ({ developer }: Props) => {
           className="rounded-full border-2 border-black"
         />
         <div className="flex grow flex-col">
-          <h2 className="text-sm font-bold md:text-2xl">{developer.name}</h2>
+          <h2 className="text-xl font-normal lg:text-2xl">{developer.name}</h2>
           <div className="flex justify-between">
-            <ul className="flex gap-1 md:gap-2">
+            <ul className="flex flex-wrap md:gap-2">
               {developer.skills.slice(0, 4).map((skill, index) => (
                 <li
-                  className="flex items-center rounded-sm bg-orange px-1 text-[0.6rem] text-white md:rounded-full md:p-1.5"
+                  className="lg:text-md flex items-center px-1 text-sm text-black/40 md:rounded-full md:p-1.5"
                   key={skill + index}
                 >
                   <p className="whitespace-nowrap">{skill}</p>
                 </li>
               ))}
               {developer.skills.length - 4 > 0 && (
-                <li className="flex items-center rounded-sm bg-orange px-1 text-[0.6rem] text-white md:rounded-full md:p-1.5">
+                <li className="flex items-center rounded-sm px-1 text-[0.6rem] text-sm text-black/40 md:p-1.5">
                   +{developer.skills.length - 4}
                 </li>
               )}
