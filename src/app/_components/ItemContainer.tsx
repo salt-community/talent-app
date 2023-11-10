4a1b8aed8a1b57c735fbaa48693cca47d700dc05
makoalex <macovei.a2@gmail.com>
2023-11-10 15:27:23 +0100

    style: added background to itemContainer

--- src/app/_components/ItemContainer.tsx
+++ src/app/_components/ItemContainer.tsx
@@ -5,7 +5,7 @@ type Props = { children: React.ReactNode; className?: string };
 const ItemContainer = ({ children, className }: Props) => {
   return (
     <li
-      className={`bg-cardBg mt-2 flex h-28 drop-shadow-md border-b-black items-center gap-2 rounded-sm p-2 lg:px-9 ${className}`}
+      className={`bg-gray mt-2 flex h-28 drop-shadow-md border-b-black items-center gap-2 rounded-sm p-2 lg:px-9 ${className}`}
     >
       {children}
     </li>
