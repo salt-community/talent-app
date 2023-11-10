a55c97d7dc4357ccdba2c8bb41e2dd95956033b6
Rasmus Eklund <49008491+rasmus-eklund@users.noreply.github.com>
2023-11-10 11:26:50 +0100

    feat: get client logs

--- src/server/api/routers/log.ts
+++ src/server/api/routers/log.ts
@@ -8,4 +8,21 @@ export const logRouter = createTRPCRouter({
       const userId = ctx.session.user.id;
       await ctx.db.logClickDev.create({ data: { developerId, userId } });
     }),
+
+  getClicks: protectedProcedure.query(async ({ ctx }) => {
+    return await ctx.db.logClickDev.findMany({
+      include: {
+        User: { select: { name: true, email: true } },
+        developer: { select: { image: true, name: true } },
+      },
+    });
+  }),
+
+  getSearch: protectedProcedure.query(async ({ ctx }) => {
+    return await ctx.db.logSearch.findMany({
+      include: {
+        User: { select: { name: true, email: true } },
+      },
+    });
+  }),
 });
