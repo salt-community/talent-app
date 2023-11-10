fe9ffc46c95f81699414464b679d1c8bd2dd0e90
makoalex <macovei.a2@gmail.com>
2023-11-10 15:14:06 +0100

    style: added title and styles to landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,8 +7,11 @@ const Home = () => {
   const [search, setSearch] = useState("");
 
   return (
-    <main className="flex flex-col gap-2 px-2 pb-2">
-      <div className="flex items-center rounded-sm bg-gradient-to-b from-orange to-pink p-2 duration-500 ease-linear md:rounded-md">
+    <main className="mt-2 flex flex-col gap-y-4 pb-2 ">
+      <div className=" flex h-20 flex-col items-center justify-center rounded-sm bg-gradient-to-r from-[rgb(255,121,97)] to-[rgb(243,92,126)] px-4 lg:h-96">
+        <h1 className="lg:font-secondary hidden lg:mb-7 lg:flex lg:text-center lg:text-5xl lg:font-bold lg:text-white">
+          Tomorrow's Full-Stack Developers. <br /> Available Today
+        </h1>
         <SearchForm onSearch={(search) => setSearch(search)} />
       </div>
       <SearchResults search={search} />
