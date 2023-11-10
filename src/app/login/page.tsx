18ea97a2afbc23ef37a270cc67c9c0682fc29ac2
Rasmus Eklund <49008491+rasmus-eklund@users.noreply.github.com>
2023-11-10 16:25:54 +0100

    Feat: email login page
    Co-authored-by: Alexandra Macovei <makoalex@users.noreply.github.com>

---
+++ src/app/login/page.tsx
@@ -0,0 +1,37 @@
+"use client";
+import { type tMail, zMail } from "@/utils/zodSchema";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { signIn } from "next-auth/react";
+import { useForm } from "react-hook-form";
+import FormError from "../_components/FormError";
+import Button from "../_components/Button";
+
+const LoginPage = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<tMail>({
+    resolver: zodResolver(zMail),
+  });
+  return (
+    <main className="flex grow items-center justify-center">
+      <form
+        className="flex max-w-sm flex-col gap-2"
+        onSubmit={(event) =>
+          void handleSubmit(async ({ email }) => {
+            console.log(email);
+            await signIn("email", { email, callbackUrl: "/" });
+          })(event)
+        }
+      >
+        <label htmlFor="email">Email address</label>
+        <input id="email" className="border p-1" {...register("email")} />
+        <FormError error={errors.email} />
+        <Button>Login</Button>
+      </form>
+    </main>
+  );
+};
+
+export default LoginPage;
