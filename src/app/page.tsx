4b8321dfc27c3850c9be65a7a5828494c9b8837d
Rasmus Eklund <49008491+rasmus-eklund@users.noreply.github.com>
2023-11-10 16:36:44 +0100

    fix: apos
    Co-authored-by: Alexandra Macovei <makoalex@users.noreply.github.com>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,7 +10,7 @@ const Home = () => {
     <main className="mt-2 flex flex-col gap-y-4 pb-2 ">
       <div className=" flex h-20 flex-col items-center justify-center rounded-sm bg-gradient-to-r from-[rgb(255,121,97)] to-[rgb(243,92,126)] px-4 lg:h-96">
         <h1 className="lg:font-secondary hidden lg:mb-7 lg:flex lg:text-center lg:text-5xl lg:font-bold lg:text-white">
-          Tomorrow's Full-Stack Developers. <br /> Available Today
+          Tomorrow&apos;s Full-Stack Developers. <br /> Available Today
         </h1>
         <SearchForm onSearch={(search) => setSearch(search)} />
       </div>
