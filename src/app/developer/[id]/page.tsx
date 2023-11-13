b1cf38e317ec01d1fd7d2814fd1cd92243805853
Rasmus Eklund <49008491+rasmus-eklund@users.noreply.github.com>
2023-11-13 10:53:22 +0100

    Style: change layout
    Co-authored-by: Allan Heremi <allanheremi@users.noreply.github.com>
    Co-authored-by: Alexandra Macovei <makoalex@users.noreply.github.com>

--- src/app/developer/[id]/page.tsx
+++ src/app/developer/[id]/page.tsx
@@ -1,56 +1,49 @@
+"use client";
 import Link from "next/link";
 import UserCard from "@/app/_components/developer/UserCard";
 import SectionHeader from "@/app/_components/SectionHeader";
 import Skills from "@/app/_components/developer/DeveloperSkills";
 import TeamMembers from "@/app/_components/developer/Team";
-import { api } from "@/trpc/server";
+import { api } from "@/trpc/react";
 import Projects from "@/app/_components/developer/Projects";
 import Contact from "@/app/_components/developer/Contact";
 import Icon from "@/app/assets/icons/Icon";
-import { env } from "@/env.mjs";
-import AddToCart from "@/app/_components/developer/AddToCart";
-import { getServerAuthSession } from "@/server/auth";
-const DeveloperPage = async ({
-  params: { id },
-}: {
-  params: { id: string };
-}) => {
-  const developer = await api.developer.getById.query({ id });
-  const session = await getServerAuthSession();
+import { useSession } from "next-auth/react";
+const DeveloperPage = ({ params: { id } }: { params: { id: string } }) => {
+  const {
+    data: developer,
+    isSuccess,
+  } = api.developer.getById.useQuery({ id });
+  const { data: session } = useSession();
   return (
-    <main className="flex grow flex-col justify-center bg-gradient-to-b from-orange to-pink px-5">
-      <div className="sticky top-0 flex w-full items-center justify-between">
-        <Link href={"/"} className="z-10 w-10">
+    <main className="flex grow flex-col items-center gap-5 bg-gradient-to-b from-orange to-pink px-5 pt-5 md:pt-0">
+      <div className="flex w-full flex-col gap-5 rounded-md bg-gray p-5 md:max-w-5xl md:rounded-none items-center">
+        <Link href={"/"} className="absolute left-2 top-16 z-10 w-10">
           <Icon
             icon="arrowLeft"
             className="h-10 rounded-full border border-black/30 bg-black fill-white active:bg-black/30"
           />
         </Link>
-        {session && session.user.role === "CLIENT" && (
-          <AddToCart developerId={id} />
+        {isSuccess && <UserCard developer={developer} />}
+      </div>
+      <div className="relative w-full flex flex-col gap-5 rounded-md bg-gray px-5 pt-5 md:max-w-5xl md:rounded-none">
+        {isSuccess && <Bio {...developer} />}
+        {session ? (
+          <>
+            {isSuccess && <Skills skills={developer.skills} />}
+            {isSuccess && <Projects projects={developer.projects} />}
+            {isSuccess && <TeamMembers mobs={developer.mobs} />}
+          </>
+        ) : (
+          <div>
+            <Link className="underline" href="/login">
+              Sign in
+            </Link>
+            <span className="ml-1">to see more...</span>
+          </div>
         )}
+        {session && isSuccess && <Contact developer={developer} />}
       </div>
-      <div className="flex grow flex-col gap-4 pb-5 md:flex-row md:py-0">
-        <UserCard developer={developer} />
-        <div className="flex flex-col gap-5 rounded-md bg-gray p-5 md:w-3/4 md:rounded-none md:px-10">
-          <Bio {...developer} />
-          {session ? (
-            <>
-              <Skills skills={developer.skills} />
-              <Projects projects={developer.projects} />
-              <TeamMembers mobs={developer.mobs} />
-            </>
-          ) : (
-            <div>
-              <Link className="underline" href="/login">
-                Sign in
-              </Link>
-              <span className="ml-1">to see more...</span>
-            </div>
-          )}
-        </div>
-      </div>
-      {env.NEXT_PUBLIC_FF_CONTACT === "ON" && <Contact developer={developer} />}
     </main>
   );
 };
