c158b3133c55f58a56ed0b13c11a690aef665165
makoalex <macovei.a2@gmail.com>
2023-11-13 15:57:26 +0100

    style: styling of the cart page

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -8,18 +8,20 @@ const Cart = async () => {
   const cart = await api.cart.getAll.query();
 
   return (
-    <main className="p-2">
-      <h2>Developers</h2>
-      <ul>
+    <main className="p-2 flex grow flex-col items-center gap-5 bg-gradient-to-b from-orange to-pink pt-5">
+      <div className="flex flex-col w-full lg:w-2/3 gap-4 rounded-sm ">
+      <h2 className="  p-2 font-light text-2xl text-center lg:text-4xl gap-y-5 ">Your Favorites</h2>
+      <ul className="">
         {cart.map(({ developerId, image, name }) => (
           <ItemContainer key={developerId} className="justify-between">
             <Link href={`developer/${developerId}`}>
-              <div className="flex items-center gap-2">
+              <div className="p-1 w-20 flex items-center gap-2">
                 <Image
                   alt="developer image"
                   src={image}
-                  height={38}
-                  width={38}
+                  height={256}
+                  width={256}
+                  className="rounded-full"
                 />
                 <p>{name}</p>
               </div>
@@ -28,6 +30,7 @@ const Cart = async () => {
           </ItemContainer>
         ))}
       </ul>
+      </div>
     </main>
   );
 };
