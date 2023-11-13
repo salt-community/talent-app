1aa35103a8290e156a2b56a9d01ea276328315e9
Rasmus Eklund <49008491+rasmus-eklund@users.noreply.github.com>
2023-11-13 10:52:08 +0100

    Style: button
    Co-authored-by: Allan Heremi <allanheremi@users.noreply.github.com>
    Co-authored-by: Alexandra Macovei <makoalex@users.noreply.github.com>

--- src/app/_components/Button.tsx
+++ src/app/_components/Button.tsx
@@ -1,7 +1,7 @@
 import type { ButtonHTMLAttributes, DetailedHTMLProps } from "react";
 
 type ButtonProps = {
-  inverted?: boolean;
+  callToAction?: boolean;
   className?: string;
 } & DetailedHTMLProps<
   ButtonHTMLAttributes<HTMLButtonElement>,
@@ -10,17 +10,17 @@ type ButtonProps = {
 
 export function Button({
   className: inClass = "",
-  inverted = false,
+  callToAction = false,
   ...props
 }: ButtonProps) {
   const className = {
-    colors: inverted
-      ? "bg-orange md:hover:bg-gray text-white md:hover:text-black"
-      : "bg-gray md:hover:bg-orange text-black md:hover:text-white",
+    colors: callToAction
+      ? "bg-orange md:hover:bg-black text-white md:hover:text-white active:bg-black"
+      : "bg-gray md:hover:bg-orange text-black md:hover:text-white active:bg-orange",
   };
   return (
     <button
-      className={`${className.colors} ${inClass} rounded-md border-2 border-black px-2 transition-colors duration-200 active:bg-orange disabled:cursor-not-allowed disabled:opacity-50`}
+      className={`${className.colors} ${inClass} lg:text-md flex items-center rounded-sm px-4 py-[6px] font-secondary text-sm tracking-wide  disabled:cursor-not-allowed disabled:opacity-50 xs:px-2 xs:py-[6px] lg:h-9 lg:px-6`}
       {...props}
     ></button>
   );
