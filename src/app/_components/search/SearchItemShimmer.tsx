ae3d4d1b937a71052deadfc9851f849c292a6be5
Rasmus Eklund <49008491+rasmus-eklund@users.noreply.github.com>
2023-11-14 05:42:49 +0100

    Refactor: shimmer

--- src/app/_components/search/SearchItemShimmer.tsx
+++ src/app/_components/search/SearchItemShimmer.tsx
@@ -1,14 +1,25 @@
-import React from 'react';
-
+import React from "react";
 
 const SearchItemShimmer = () => {
   return (
     <>
-    <div className="p-4 bg-gray rounded-lg shadow-lg animate-pulse duration-500 h-[9rem] my-2">
-      <div className=' h-12 w-12 rounded-full bg-black/10 mb-4'></div>
-      <div className="h-4 bg-black/10 rounded w-4/5 mb-4"></div>
-      <div className="h-4 bg-black/10 rounded w-3/5"></div>
-    </div>
+      {Array(10)
+        .fill(1)
+        .map(() => {
+          const width = Math.floor(Math.random() * 11) + 1;
+          return (
+            <li
+              key={crypto.randomUUID()}
+              className="flex h-28 animate-pulse items-center gap-4 rounded-sm bg-gray p-2 drop-shadow-md duration-500 lg:px-9"
+            >
+              <div className="h-20 w-20 shrink-0 rounded-full bg-black/10 p-2"></div>
+              <div className="flex h-full w-full flex-col justify-evenly">
+                <div className="h-4 w-2/5 rounded bg-black/10"></div>
+                <div className={`h-4 w-${width}/12 rounded bg-black/10`}></div>
+              </div>
+            </li>
+          );
+        })}
     </>
   );
 };
