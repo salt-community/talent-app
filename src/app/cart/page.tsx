6873f96b0a6032490539052252b5f05fb1981669
Rasmus Eklund <49008491+rasmus-eklund@users.noreply.github.com>
2023-11-14 16:41:12 +0100

    Feat: use client component
    Co-authored-by: Allan Heremi <allanheremi@users.noreply.github.com>
    Co-authored-by: Alexandra Macovei <makoalex@users.noreply.github.com>
    Co-authored-by: Lee Sheppard <coolusername244@users.noreply.github.com>

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -1,41 +1,48 @@
-import { api } from "@/trpc/server";
+"use client";
+import { api } from "@/trpc/react";
 import ItemContainer from "../_components/ItemContainer";
 import Image from "next/image";
 import Link from "next/link";
 import RemoveFromCart from "./components/RemoveFromCart";
 
-const Cart = async () => {
-  const cart = await api.cart.getAll.query();
+const Cart = () => {
+  const {
+    data: cart,
+    isSuccess,
+    isLoading,
+    isError,
+  } = api.cart.getAll.useQuery();
 
   return (
     <main className="flex grow flex-col items-center gap-5 bg-gradient-to-b from-orange to-pink p-2 pt-5">
-      <div className="flex w-full flex-col gap-4 rounded-sm lg:w-2/3 ">
+      <div className="flex w-full flex-col gap-4 rounded-sm lg:w-2/3">
         <h2 className="gap-y-5 p-2 text-center text-2xl font-light lg:text-4xl">
           Your Favorites
         </h2>
         <ul className="flex flex-col gap-2">
-          {cart.map(({ developerId, image, name }) => (
-            <ItemContainer key={developerId} className="p-0">
-              <Link
-                href={`developer/${developerId}`}
-                className="flex justify-between w-full p-2"
-              >
-                <div className="flex items-center gap-4">
-
-                <Image
-                  alt="developer image"
-                  src={image}
-                  height={256}
-                  width={256}
-                  className="w-20 rounded-full"
-                  />
-                <p className="text-lg">{name}</p>
+          {isSuccess &&
+            cart.map(({ developerId, image, name }) => (
+              <ItemContainer key={developerId} className="px-4">
+                <Link
+                  href={`developer/${developerId}`}
+                  className="flex w-full justify-between"
+                >
+                  <div className="flex items-center gap-4">
+                    <Image
+                      alt="developer image"
+                      src={image}
+                      height={256}
+                      width={256}
+                      className="w-20 rounded-full"
+                    />
+                    <p className="text-lg">{name}</p>
                   </div>
+                </Link>
                 <RemoveFromCart developerId={developerId} />
-              </Link>
-              
-            </ItemContainer>
-          ))}
+              </ItemContainer>
+            ))}
+          {isLoading && <p>Loading...</p>}
+          {isError && <p>Something went wrong...</p>}
         </ul>
       </div>
     </main>
