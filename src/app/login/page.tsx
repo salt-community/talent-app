e5910abb890fe24da809da1078b7432b00fd4114
Rasmus Eklund <49008491+rasmus-eklund@users.noreply.github.com>
2023-11-16 13:07:43 +0100

    Style: login page
    Co-authored-by: Allan Heremi <allanheremi@users.noreply.github.com>
    Co-authored-by: Alexandra Macovei <makoalex@users.noreply.github.com>
    Co-authored-by: Lee Sheppard <coolusername244@users.noreply.github.com>

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -11,40 +11,50 @@ const LoginPage = () => {
     register,
     handleSubmit,
     formState: { errors },
+    watch,
   } = useForm<tMail>({
     resolver: zodResolver(zMail),
   });
+  const watchInput = watch("email");
   return (
     <main className="flex grow items-center justify-center">
-      <div className=" flex relative flex-col justify-center items-center h-[350px] mx-3 w-screen lg:w-2/4 lg:h-[450px] rounded-md shadow-md bg-gray">
-      <h2 className=" font-secondary absolute top-10 font-light text-lg lg:text-2xl"> Please Sign in </h2>
-        <h3 className=" font-secondary font-light absolute top-20 text-md lg:text-xl"> To get access to our Salties</h3>
-      <form
-        className="flex max-w-sm flex-col gap-3 "
-        onSubmit={(event) =>
-          void handleSubmit(async ({ email }) => {
-            await signIn("email", { email, callbackUrl: "/" });
-          })(event)
-        }
-      >
-        <div className="relative mb-2">
-          <input
-            className="input w-full block border-b-2 border-black/40 bg-transparent p-2 text-sm focus:outline-none"
-            id="email"
-            {...register("email")}
-          />
-          <span className="highlight"></span>
-          <span className="bar relative block w-52"></span>
-          <label
-            className="label pointer-events-none absolute left-2 top-1 text-base text-black/40 transition-all duration-500 ease-in-out"
-            htmlFor="email"
-          >
-            Email address
-          </label>
-          <FormError error={errors.email} />
-        </div>
+      <div className=" relative flex h-[350px] w-screen flex-col items-center justify-center rounded-md bg-gray shadow-md lg:h-[450px] lg:w-2/4">
+        <h2 className=" absolute top-10 font-secondary text-lg font-light lg:text-2xl">
+          Please Sign in
+        </h2>
+        <h3 className="text-md absolute top-20 font-secondary font-light lg:text-xl">
+          To get access to our Salties
+        </h3>
+        <form
+          className="flex max-w-sm w-3/4 md:1/2 flex-col gap-4"
+          onSubmit={(event) =>
+            void handleSubmit(async ({ email }) => {
+              await signIn("email", { email, callbackUrl: "/" });
+            })(event)
+          }
+        >
+          <div className="relative">
+            <input
+              className="peer w-full border-b-2 border-black/40 bg-transparent p-2 text-sm outline-none"
+              id="email"
+              {...register("email")}
+            />
+            <span className="absolute bottom-0 left-0 block h-[2px] w-0 transition-[width] duration-500 peer-focus:w-1/2 peer-focus:bg-orange"></span>
+            <span className="absolute bottom-0 right-0 block h-[2px] w-0 transition-[width] duration-500 peer-focus:w-1/2 peer-focus:bg-orange"></span>
+            <label
+              className={`pointer-events-none absolute left-2 top-1 text-base text-black/40 transition-all duration-500 ease-in-out peer-focus:-translate-y-5 peer-focus:scale-110 peer-focus:text-orange ${
+                watchInput &&
+                watchInput.length !== 0 &&
+                "-translate-y-5 scale-110 text-orange"
+              }`}
+              htmlFor="email"
+            >
+              Email address
+            </label>
+            <FormError error={errors.email} />
+          </div>
           <Button callToAction>Login</Button>
-      </form>
+        </form>
       </div>
     </main>
   );
