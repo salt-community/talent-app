b5ff97efb3af3c36e1a434aa2f161069204c4fd8
Rasmus Eklund <49008491+rasmus-eklund@users.noreply.github.com>
2023-11-17 13:53:31 +0100

    Fix: don't check for unique. Too few data

--- prisma/addSlug.ts
+++ prisma/addSlug.ts
@@ -2,13 +2,7 @@ import { db } from "@/server/db";
 export const addSlug = async () => {
   const devs = await db.developer.findMany();
   for (const dev of devs) {
-    let slug = dev.name.toLowerCase().replace(" ", "-");
-    const results = await db.developer.findMany({
-      where: { slug: { equals: slug } },
-    });
-    if (results.length !== 0) {
-      slug = crypto.randomUUID();
-    }
+    const slug = dev.name.toLowerCase().replace(/[^a-zA-Z]+/g, "-");
     await db.developer.update({ where: { id: dev.id }, data: { slug } });
   }
 };
