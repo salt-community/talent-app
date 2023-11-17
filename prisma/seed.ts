a07bbd6c85969cb3b9ade1108d8be3746aa042ea
Rasmus Eklund <49008491+rasmus-eklund@users.noreply.github.com>
2023-11-17 13:53:48 +0100

    Refactor: default - do nothing

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,16 +1,16 @@
-import seedMeilisearch from "@/server/seedMeilisearch";
+// import seedMeilisearch from "@/server/seedMeilisearch";
 // import { storeDataLocally, seedDataToDb } from "./backup";
 
 // import { addSlug } from "./addSlug";
 
 const seedData = async () => {
-  await seedMeilisearch();
+  // await seedMeilisearch();
   // await storeDataLocally();
   // await seedDataToDb();
   // await addSlug();
-  // await new Promise((resolve) => {
-  //   resolve(console.log("No script to run."));
-  // });
+  await new Promise((resolve) => {
+    resolve(console.log("No script to run."));
+  });
 };
 
 seedData().catch(() => console.log("Seed failed!"));
