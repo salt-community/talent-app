3bffd187f834a300a0aea8dc8d2d4b71a40d551e
Rasmus Eklund <49008491+rasmus-eklund@users.noreply.github.com>
2023-11-20 11:41:03 +0100

    Refactor: change input type
    Co-authored-by: Allan Heremi <allanheremi@users.noreply.github.com>

--- src/app/profile/components/MobItem.tsx
+++ src/app/profile/components/MobItem.tsx
@@ -1,13 +1,12 @@
 import Link from "next/link";
-import type { RouterOutputs } from "@/trpc/shared";
 import ItemContainer from "../../_components/ItemContainer";
-type Mob = RouterOutputs["mob"]["getByDev"][number];
 
 type Props = {
-  mob: Mob;
+  id: string;
+  name: string;
 };
 
-const MobItem = ({ mob: { id, name } }: Props) => {
+const MobItem = ({ id, name }: Props) => {
   return (
     <Link href={`/profile/mob/${id}`}>
       <ItemContainer className="px-5">
