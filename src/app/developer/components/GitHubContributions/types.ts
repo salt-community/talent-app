8fae34c09c857a399a8aa2905803c9ce4c78b7eb
Rasmus Eklund <49008491+rasmus-eklund@users.noreply.github.com>
2023-11-20 15:48:04 +0100

    Feat: add github contributions calendar
    Co-authored-by: Allan Heremi <allanheremi@users.noreply.github.com>

---
+++ src/app/developer/components/GitHubContributions/types.ts
@@ -0,0 +1,15 @@
+import type { Activity } from "react-activity-calendar";
+
+export type Year = number | "last";
+
+export interface ApiResponse {
+  total: {
+    [year: number]: number;
+    [year: string]: number; // 'lastYear;
+  };
+  contributions: Array<Activity>;
+}
+
+export interface ApiErrorResponse {
+  error: string;
+}
