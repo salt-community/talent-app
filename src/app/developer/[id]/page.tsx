2e1a17f3901add17580d035fe9a412f4b7be340d
Rasmus Eklund <49008491+rasmus-eklund@users.noreply.github.com>
2023-11-20 17:02:47 +0100

    Refactor: move shimmers into components
    Co-authored-by: Allan Heremi <allanheremi@users.noreply.github.com>

--- src/app/developer/[id]/page.tsx
+++ src/app/developer/[id]/page.tsx
@@ -1,6 +1,5 @@
 "use client";
 import UserCard from "@/app/developer/components/UserCard";
-import SectionHeader from "@/app/developer/components/SectionHeader";
 import Skills from "@/app/developer/components/Skills";
 import TeamMembers from "@/app/developer/components/Team";
 import { api } from "@/trpc/react";
@@ -9,9 +8,11 @@ import Contact from "@/app/developer/components/Contact";
 import Icon from "@/app/assets/icons/Icon";
 import { useSession } from "next-auth/react";
 import UserCardShimmer from "../components/shimmer/UserCardShimmer";
-import DeveloperCardShimmer from "../components/shimmer/DeveloperCardShimmer";
 import { useRouter } from "next/navigation";
 import GitHubCalendar from "../components/GitHubContributions/GitHubContributions";
+import SkillsShimmer from "../components/shimmer/SkillsShimmer";
+import type { ReactNode } from "react";
+import GitHubShimmer from "../components/shimmer/GitHubShimmer";
 
 const DeveloperPage = ({ params: { id } }: { params: { id: string } }) => {
   const {
@@ -27,7 +28,7 @@ const DeveloperPage = ({ params: { id } }: { params: { id: string } }) => {
         !session && "pb-5"
       }`}
     >
-      <div className="relative flex w-full flex-col items-center gap-5 bg-gray p-5 md:max-w-5xl md:rounded-md">
+      <section className="relative flex w-full flex-col items-center gap-5 bg-gray p-5 md:max-w-5xl md:rounded-md">
         <button
           onClick={() => router.back()}
           className="absolute left-2 top-2 w-10"
@@ -36,36 +37,48 @@ const DeveloperPage = ({ params: { id } }: { params: { id: string } }) => {
         </button>
         {isSuccess && <UserCard developer={developer} />}
         {isLoading && <UserCardShimmer />}
-      </div>
-      <div className="flex w-full grow flex-col gap-5 bg-gray px-5 pt-5 md:max-w-5xl md:rounded-md">
-        {isSuccess && <Skills skills={developer.skills} />}
-        {isSuccess && <Bio {...developer} />}
-        {isLoading && <DeveloperCardShimmer />}
-        {isSuccess && <Projects projects={developer.projects} />}
-        {isSuccess && developer.gitHubUsername && (
-          <>
-            <SectionHeader title="Github contributions" />
-            <GitHubCalendar
-              username={developer.gitHubUsername}
-              fontSize={16}
-              colorScheme="light"
-            />
-          </>
-        )}
-        {isSuccess && <TeamMembers mobs={developer.mobs} />}
-      </div>
+      </section>
+      <section className="flex w-full grow flex-col gap-5 bg-gray px-5 pt-5 md:max-w-5xl md:rounded-md">
+        <Article title="Skills">
+          {isSuccess && <Skills skills={developer.skills} />}
+          {isLoading && <SkillsShimmer />}
+        </Article>
+        <Article title="GitHub contributions">
+          {isSuccess && developer.gitHubUsername && (
+            <>
+              <GitHubCalendar
+                username={developer.gitHubUsername}
+                fontSize={16}
+                colorScheme="light"
+              />
+            </>
+          )}
+          {isLoading && <GitHubShimmer />}
+        </Article>
+        <Article title={developer?.title ?? "Fullstack web developer"}>
+          {isSuccess && <p>{developer.description}</p>}
+        </Article>
+        <Article title="Projects">
+          {isSuccess && <Projects projects={developer.projects} />}
+        </Article>
+        <Article title="Team members">
+          {isSuccess && developer.mobs.length > 0 && (
+            <TeamMembers mob={developer.mobs[0]!} />
+          )}
+        </Article>
+      </section>
       {session && isSuccess && <Contact developer={developer} />}
     </main>
   );
 };
 
-type BioProps = { title: string; description: string };
-const Bio = ({ title, description }: BioProps) => {
+type ArticleProps = { title: string; children: ReactNode };
+const Article = ({ title, children }: ArticleProps) => {
   return (
-    <section className="flex flex-col gap-4">
-      <SectionHeader title={title} />
-      <p>{description}</p>
-    </section>
+    <article className="flex flex-col gap-4">
+      <h2 className="text-xl font-medium md:text-3xl">{title}</h2>
+      {children}
+    </article>
   );
 };
 
