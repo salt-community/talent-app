f32a7bafc8c05e848fee9f88473d68d41fe3ec05
Rasmus Eklund <49008491+rasmus-eklund@users.noreply.github.com>
2023-11-21 11:23:27 +0100

    Refactor: moved shimmers into respective component
    Co-authored-by: Allan Heremi <allanheremi@users.noreply.github.com>

--- src/app/developer/components/Projects.tsx
+++ src/app/developer/components/Projects.tsx
@@ -1,38 +1,49 @@
 import type { RouterOutputs } from "@/trpc/shared";
 import SectionHeader from "./SectionHeader";
 import Icon from "@/app/assets/icons/Icon";
-type Project = RouterOutputs["developer"]["getBySlug"]["projects"][number];
+import type { LoadingProps } from "types";
+type Developer = RouterOutputs["developer"]["getBySlug"];
 
-type Props = {
-  projects: Project[];
-};
-const Projects = ({ projects }: Props) => {
-  return (
-    <ul className="flex flex-col gap-4">
-      {projects.map((project) => (
-        <div key={project.id} className="flex flex-col gap-4">
-          <div className="flex items-center gap-2">
-            <SectionHeader title={project.title} />{" "}
-            <a className="w-8" href={project.githublink} target="_blank">
-              <Icon icon="github" className="h-8 w-8 fill-black" />
-            </a>
+const Projects = ({ data }: LoadingProps<Developer>) => {
+  if (data.status === "loading") {
+    return (
+      <div className="relative pb-[56.25%] bg-black/10 animate-pulse">
+        <div className="absolute left-0 top-0 h-full w-full"></div>
+      </div>
+    );
+  }
+  if (data.status === "success") {
+    const projects = data.data.projects;
+    return (
+      <ul className="flex flex-col gap-4">
+        {projects.map((project) => (
+          <div key={project.id} className="flex flex-col gap-4">
+            <div className="flex items-center gap-2">
+              <SectionHeader title={project.title} />{" "}
+              <a className="w-8" href={project.githublink} target="_blank">
+                <Icon icon="github" className="h-8 w-8 fill-black" />
+              </a>
+            </div>
+            <div className="relative pb-[56.25%]">
+              <iframe
+                className="absolute left-0 top-0 h-full w-full"
+                src={`https://www.youtube.com/embed/${project.youtube}`}
+                title={project.title}
+                width={"100%"}
+                height={"100%"}
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                allowFullScreen
+              ></iframe>
+            </div>
+            <p>{project.description}</p>
           </div>
-          <div className="relative pb-[56.25%]">
-            <iframe
-              className="absolute left-0 top-0 h-full w-full"
-              src={`https://www.youtube.com/embed/${project.youtube}`}
-              title={project.title}
-              width={"100%"}
-              height={"100%"}
-              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-              allowFullScreen
-            ></iframe>
-          </div>
-          <p>{project.description}</p>
-        </div>
-      ))}
-    </ul>
-  );
+        ))}
+      </ul>
+    );
+  }
+  if (data.status === "error") {
+    return <p>Something went wrong...</p>;
+  }
 };
 
 export default Projects;
--- src/app/developer/components/Skills.tsx
+++ src/app/developer/components/Skills.tsx
@@ -1,34 +1,54 @@
 "use client";
+import type { RouterOutputs } from "@/trpc/shared";
 import { useState } from "react";
-type Props = { skills: string[] };
+import type { LoadingProps } from "types";
+type Developer = RouterOutputs["developer"]["getBySlug"];
 
-const Skills = ({ skills }: Props) => {
+const Skills = ({ data }: LoadingProps<Developer>) => {
   const [more, setMore] = useState(false);
-  return (
-    <ul className="flex flex-wrap gap-3">
-      {skills
-        .filter((_, i) => more || i < 4)
-        .map((skill, index) => {
-          return (
-            <li key={skill + index}>
-              <p className="rounded-full bg-orange/10 px-2 py-1 text-sm text-black/70">
-                {skill}
-              </p>
-            </li>
-          );
-        })}
-      <li>
-        {skills.length > 4 && (
-          <button
-            className="text-sm underline underline-offset-2"
-            onClick={() => setMore((p) => !p)}
-          >
-            {more ? "Show less" : "Show more"}
-          </button>
-        )}
-      </li>
-    </ul>
-  );
+  if (data.status === "loading") {
+    return (
+      <div className="flex w-full animate-pulse gap-2">
+        <div className="h-4 w-1/5 rounded bg-black/10"></div>
+        <div className="h-4 w-1/5 rounded bg-black/10"></div>
+        <div className="h-4 w-1/5 rounded bg-black/10"></div>
+        <div className="h-4 w-1/5 rounded bg-black/10"></div>
+      </div>
+    );
+  }
+
+  if (data.status === "success") {
+    const skills = data.data.skills;
+    return (
+      <ul className="flex flex-wrap gap-3">
+        {skills
+          .filter((_, i) => more || i < 4)
+          .map((skill, index) => {
+            return (
+              <li key={skill + index}>
+                <p className="rounded-full bg-orange/10 px-2 py-1 text-sm text-black/70">
+                  {skill}
+                </p>
+              </li>
+            );
+          })}
+        <li>
+          {skills.length > 4 && (
+            <button
+              className="text-sm underline underline-offset-2"
+              onClick={() => setMore((p) => !p)}
+            >
+              {more ? "Show less" : "Show more"}
+            </button>
+          )}
+        </li>
+      </ul>
+    );
+  }
+
+  if (data.status === "error") {
+    return <p>Something went wrong</p>;
+  }
 };
 
 export default Skills;
