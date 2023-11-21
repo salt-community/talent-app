154f3ca50360bbfe68719eed7879f95b0f18910d
Rasmus Eklund <49008491+rasmus-eklund@users.noreply.github.com>
2023-11-21 15:40:45 +0100

    Refactor: protect page
    Co-authored-by: Allan Heremi <allanheremi@users.noreply.github.com>

--- src/app/admin/users/page.tsx
+++ src/app/admin/users/page.tsx
@@ -1,16 +1,25 @@
 import { api } from "@/trpc/server";
 import Users from "./components/Users";
 import { getServerAuthSession } from "@/server/auth";
-import NotAllowed from "../components/notAllowed";
 const page = async () => {
   const session = await getServerAuthSession();
+  if (!session) {
+    return (
+      <main className="flex grow items-center justify-center">
+        <p>You are not allowed here!</p>
+      </main>
+    );
+  }
+  if (session && session.user.role !== "ADMIN") {
+    <main className="flex grow items-center justify-center">
+      <p>You are not allowed here {session.user.role}!</p>
+    </main>;
+  }
   const users = await api.users.getAll.query();
   return (
-    <NotAllowed session={session}>
-      <main>
-        <Users users={users} />
-      </main>
-    </NotAllowed>
+    <main className="flex flex-col gap-2 p-5">
+      <Users users={users} />
+    </main>
   );
 };
 
