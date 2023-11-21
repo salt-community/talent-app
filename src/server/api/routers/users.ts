b04b454c6835ab0fbd61eaa9bfeba753bddd6599
Rasmus Eklund <49008491+rasmus-eklund@users.noreply.github.com>
2023-11-21 15:42:33 +0100

    Feat: get client interactions aggregated
    Co-authored-by: Allan Heremi <allanheremi@users.noreply.github.com>

--- src/server/api/routers/users.ts
+++ src/server/api/routers/users.ts
@@ -1,6 +1,7 @@
 import { zRole } from "@/utils/zodSchema";
 import { createTRPCRouter, protectedProcedure } from "../trpc";
 import { z } from "zod";
+import { TRPCError } from "@trpc/server";
 
 export const usersRouter = createTRPCRouter({
   getAll: protectedProcedure.query(async ({ ctx }) => {
@@ -19,4 +20,25 @@ export const usersRouter = createTRPCRouter({
         await ctx.db.user.update({ where: { id }, data: { role } });
       },
     ),
+
+  getInteractionsById: protectedProcedure
+    .input(z.object({ id: z.string().cuid() }))
+    .query(async ({ ctx, input: { id } }) => {
+      const user = await ctx.db.user.findUnique({
+        where: { id },
+        include: {
+          cartItems: {
+            select: { date: true, developer: { select: { name: true } } },
+          },
+          LogClickDev: {
+            select: { date: true, developer: { select: { name: true } } },
+          },
+          LogSearch: { select: { search: true, date: true } },
+        },
+      });
+      if (!user) {
+        throw new TRPCError({ code: "NOT_FOUND", message: "User not found" });
+      }
+      return user;
+    }),
 });
