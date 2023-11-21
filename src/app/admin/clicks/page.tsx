b6843d82a44fc775cd05b923fbb53c8c7b6ca235
Rasmus Eklund <49008491+rasmus-eklund@users.noreply.github.com>
2023-11-21 15:39:21 +0100

    Refactor: use server and protect
    Co-authored-by: Allan Heremi <allanheremi@users.noreply.github.com>

--- src/app/admin/clicks/page.tsx
+++ src/app/admin/clicks/page.tsx
@@ -1,14 +1,7 @@
-"use client";
-import { api } from "@/trpc/react";
-import { useSession } from "next-auth/react";
-
-const Admin = () => {
-  const { data: session } = useSession();
-  if (session && session.user.role !== "ADMIN") {
-    <main className="flex grow items-center justify-center">
-      <p>You are not allowed here {session.user.role}!</p>
-    </main>;
-  }
+import { getServerAuthSession } from "@/server/auth";
+import { api } from "@/trpc/server";
+const Admin = async () => {
+  const session = await getServerAuthSession();
   if (!session) {
     return (
       <main className="flex grow items-center justify-center">
@@ -16,41 +9,45 @@ const Admin = () => {
       </main>
     );
   }
-  const {data: clicks, isSuccess} = api.log.getClicks.useQuery();
+  if (session && session.user.role !== "ADMIN") {
+    <main className="flex grow items-center justify-center">
+      <p>You are not allowed here {session.user.role}!</p>
+    </main>;
+  }
+  const clicks = await api.log.getClicks.query();
   const className = "border text-xs md:text-base";
- 
   return (
     <main className="flex flex-col gap-2 p-5">
-     <section className="flex flex-col">
-      <h2>Clicked on developers</h2>
-      <table>
-        <thead>
-          <tr>
-            <td className={className}>Date</td>
-            <td className={className}>Mail</td>
-            <td className={className}>Developer</td>
-          </tr>
-        </thead>
-        <tbody>
-          {isSuccess && clicks.map((i) => (
-            <tr key={i.id}>
-              <td className={className}>
-                {i.date.toLocaleTimeString("sv-SE", {
-                  year: "numeric",
-                  month: "2-digit",
-                  day: "2-digit",
-                  hour: "2-digit",
-                  minute: "2-digit",
-                  second: "2-digit",
-                })}
-              </td>
-              <td className={className}>{i.User.email}</td>
-              <td className={className}>{i.developer.name}</td>
+      <section className="flex flex-col">
+        <h2>Clicked on developers</h2>
+        <table>
+          <thead>
+            <tr>
+              <td className={className}>Date</td>
+              <td className={className}>Mail</td>
+              <td className={className}>Developer</td>
             </tr>
-          ))}
-        </tbody>
-      </table>
-    </section>
+          </thead>
+          <tbody>
+            {clicks.map((i) => (
+              <tr key={i.id}>
+                <td className={className}>
+                  {i.date.toLocaleTimeString("sv-SE", {
+                    year: "numeric",
+                    month: "2-digit",
+                    day: "2-digit",
+                    hour: "2-digit",
+                    minute: "2-digit",
+                    second: "2-digit",
+                  })}
+                </td>
+                <td className={className}>{i.User.email}</td>
+                <td className={className}>{i.developer.name}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </section>
     </main>
   );
 };
