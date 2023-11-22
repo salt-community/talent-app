7358ea33faa47ca613e7c889ccae8bd724100b96
Rasmus Eklund <49008491+rasmus-eklund@users.noreply.github.com>
2023-11-22 17:16:10 +0100

    Refactor: moved html elements
    Co-authored-by: Allan Heremi <allanheremi@users.noreply.github.com>

--- src/app/developer/[id]/page.tsx
+++ src/app/developer/[id]/page.tsx
@@ -5,9 +5,7 @@ import TeamMembers from "@/app/developer/components/Team";
 import { api } from "@/trpc/react";
 import Projects from "@/app/developer/components/Projects";
 import Contact from "@/app/developer/components/Contact";
-import Icon from "@/app/assets/icons/Icon";
 import { useSession } from "next-auth/react";
-import { useRouter } from "next/navigation";
 import GitHubCalendar from "../components/GitHubContributions/GitHubContributions";
 import type { ReactNode } from "react";
 
@@ -15,22 +13,13 @@ const DeveloperPage = ({ params: { id } }: { params: { id: string } }) => {
   const { data: developer, status } = api.developer.getBySlug.useQuery({ id });
   const { data: session } = useSession();
   const gitHubUsername = developer ? developer.gitHubUsername : null;
-  const router = useRouter();
   return (
     <main
       className={`flex grow flex-col items-center gap-5 bg-gradient-to-b from-orange to-pink pt-5 md:px-5 ${
         !session && "pb-5"
       }`}
     >
-      <section className="relative flex w-full flex-col items-center gap-5 bg-gray p-5 md:max-w-5xl md:rounded-md">
-        <button
-          onClick={() => router.back()}
-          className="absolute left-2 top-2 w-10"
-        >
-          <Icon icon="arrowLeft" className="h-10 fill-black" />
-        </button>
-        <UserCard data={{ data: developer!, status }} />
-      </section>
+      <UserCard data={{ data: developer!, status }} />
       <section className="flex w-full grow flex-col gap-5 bg-gray px-5 pt-5 md:max-w-5xl md:rounded-md">
         <Article title="Skills">
           <Skills data={{ data: developer!, status }} />
