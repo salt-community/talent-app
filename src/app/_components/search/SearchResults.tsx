11778a5931f009cfda607ef7e3efce3252e0f96a
Rasmus Eklund <49008491+rasmus-eklund@users.noreply.github.com>
2023-11-22 17:15:50 +0100

    Feat: store scroll position and search to local storage
    Co-authored-by: Allan Heremi <allanheremi@users.noreply.github.com>

--- src/app/_components/search/SearchResults.tsx
+++ src/app/_components/search/SearchResults.tsx
@@ -2,12 +2,25 @@
 import { api } from "@/trpc/react";
 import SearchItemShimmer from "./SearchItemShimmer";
 import SearchItem from "./SearchItem";
+import { useRef } from "react";
+import type { RouterOutputs } from "@/trpc/shared";
+type Dev = RouterOutputs["developer"]["getBySearch"][number];
 
 type Props = {
   search: string;
 };
-
 const SearchResults = ({ search }: Props) => {
+  const scrollRef = useRef<HTMLUListElement>(null);
+  const handleLocalStorage = (devs: Dev[]) => {
+    localStorage.setItem(
+      "next-devs",
+      JSON.stringify({
+        search,
+        scrollPosition: scrollRef.current ? scrollRef.current.scrollTop : 0,
+        devs: devs.map(({ slug }) => ({ slug })),
+      }),
+    );
+  };
   const {
     data: developers,
     isSuccess: gotDevs,
@@ -23,19 +36,23 @@ const SearchResults = ({ search }: Props) => {
     },
   );
   if (gotDevs) {
-    localStorage.setItem(
-      "next-devs",
-      JSON.stringify(developers.map(({ slug }) => ({ name: slug }))),
-    );
   }
   return (
-    <ul data-cy="dev-list" className="flex w-full max-w-5xl flex-col gap-4 p-2">
+    <ul
+      ref={scrollRef}
+      data-cy="dev-list"
+      className="flex h-full w-full max-w-5xl flex-col gap-4 overflow-y-auto p-2"
+    >
       {gotDevs && developers.length === 0 && (
         <p data-cy="no-listings">No results found...</p>
       )}
       {gotDevs &&
-        developers.map((developer) => (
-          <SearchItem key={developer.id} developer={developer} />
+        [...developers].map((developer) => (
+          <SearchItem
+            key={developer.id}
+            developer={developer}
+            storeScroll={() => handleLocalStorage(developers)}
+          />
         ))}
       {gettingDevs && <SearchItemShimmer />}
       {errorDevs && <p>Something went wrong...</p>}
