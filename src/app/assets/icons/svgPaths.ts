8fae1ec1fa61552eaa6db19a83a3bd27aeed1bd1
Rasmus Eklund <49008491+rasmus-eklund@users.noreply.github.com>
2023-11-23 10:33:08 +0100

    Feat: close icon
    Co-authored-by: Allan Heremi <allanheremi@users.noreply.github.com>

--- src/app/assets/icons/svgPaths.ts
+++ src/app/assets/icons/svgPaths.ts
@@ -28,6 +28,8 @@ const svgPaths = {
   nextPerson: "M8.59,16.58L13.17,12L8.59,7.41L10,6L16,12L10,18L8.59,16.58Z",
   previousPerson:
     "M15.41,16.58L10.83,12L15.41,7.41L14,6L8,12L14,18L15.41,16.58Z",
+  close:
+    "M19,6.41L17.59,5L12,10.59L6.41,5L5,6.41L10.59,12L5,17.59L6.41,19L12,13.41L17.59,19L19,17.59L13.41,12L19,6.41Z",
 } as const;
 
 export default svgPaths;
