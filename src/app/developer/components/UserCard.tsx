f4b2732bdea34d3d3cbe74e1a00cca82bad78c9b
Rasmus Eklund <49008491+rasmus-eklund@users.noreply.github.com>
2023-11-23 09:20:40 +0100

    Style: fix shimmer
    Co-authored-by: Allan Heremi <allanheremi@users.noreply.github.com>

--- src/app/developer/components/UserCard.tsx
+++ src/app/developer/components/UserCard.tsx
@@ -19,10 +19,13 @@ const UserCard = ({ data }: LoadingProps<Developer>) => {
   const router = useRouter();
   if (data.status === "loading") {
     return (
-      <div className="my-2 flex h-[12rem] w-[12rem] animate-pulse flex-col items-center rounded-lg bg-gray p-4">
-        <div className=" mb-4 h-24 w-24 rounded-full bg-black/10"></div>
-        <div className="mb-4 h-4 w-full rounded bg-black/10"></div>
-        <div className="h-4 w-full rounded bg-black/10"></div>
+      <div className="flex h-full w-full items-center justify-center rounded-lg bg-gray py-2">
+        <div className="flex animate-pulse flex-col gap-2">
+          <div className="h-24 w-24 rounded-full bg-black/10"></div>
+          <div className="h-4 w-full rounded bg-black/10"></div>
+          <div className="h-4 w-full rounded bg-black/10"></div>
+          <div className="h-4 w-full rounded bg-black/10"></div>
+        </div>
       </div>
     );
   }
