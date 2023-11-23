b9741321bc955cd1f82499d2415b393f18f05c9d
Rasmus Eklund <49008491+rasmus-eklund@users.noreply.github.com>
2023-11-23 16:28:11 +0100

    Feat: add comment to favorite
    Co-authored-by: Allan Heremi <allanheremi@users.noreply.github.com>

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -3,7 +3,11 @@ import { api } from "@/trpc/react";
 import ItemContainer from "../_components/ItemContainer";
 import Image from "next/image";
 import Link from "next/link";
-import RemoveFromCart from "./components/RemoveFromCart";
+import { useForm } from "react-hook-form";
+import { Fragment, useState } from "react";
+import Icon from "../assets/icons/Icon";
+import type { RouterOutputs } from "@/trpc/shared";
+type Developer = RouterOutputs["cart"]["getAll"][number];
 
 const Cart = () => {
   const {
@@ -14,38 +18,102 @@ const Cart = () => {
   } = api.cart.getAll.useQuery();
 
   return (
-    <main className="flex grow flex-col items-center gap-5 bg-gradient-to-b from-orange to-pink p-2 pt-5">
-      <div className="flex w-full flex-col gap-4 rounded-sm lg:w-2/3">
-        <h2 className="gap-y-5 p-2 text-center text-2xl font-light lg:text-4xl">
-          Your Favorites
-        </h2>
-        <ul className="flex flex-col gap-2">
-          {isSuccess &&
-            cart.map(({ developerId, image, name, slug }) => (
-              <ItemContainer key={developerId} className="px-4">
-                <Link
-                  href={`developer/${slug}`}
-                  className="flex w-full justify-between"
-                >
-                  <div className="flex items-center gap-4">
-                    <Image
-                      alt="developer image"
-                      src={image}
-                      height={256}
-                      width={256}
-                      className="w-20 rounded-full"
-                    />
-                    <p className="text-lg">{name}</p>
-                  </div>
-                </Link>
-                <RemoveFromCart developerId={developerId} />
-              </ItemContainer>
-            ))}
-          {isLoading && <p>Loading...</p>}
-          {isError && <p>Something went wrong...</p>}
-        </ul>
-      </div>
+    <main className="flex grow flex-col items-center gap-5 bg-gradient-to-b from-orange to-pink pt-5">
+      <h2 className="gap-y-5 p-2 text-center text-2xl font-light lg:text-4xl">
+        Your Favorites
+      </h2>
+      <ul className="flex w-full flex-col gap-2 p-2 lg:w-2/3">
+        {isSuccess &&
+          cart.map((dev) => <CartItem key={dev.developerId} developer={dev} />)}
+        {isLoading && <p>Loading...</p>}
+        {isError && <p>Something went wrong...</p>}
+      </ul>
     </main>
+  );
+};
+
+type CartItemProps = { developer: Developer };
+const CartItem = ({
+  developer: { developerId, comment, slug, image, name },
+}: CartItemProps) => {
+  const { register, handleSubmit } = useForm<{ comment: string }>({
+    defaultValues: { comment: comment ? comment : "" },
+  });
+  const utils = api.useContext();
+  const { mutate: updateComment } = api.cart.updateComment.useMutation({
+    onSuccess: async () => {
+      setEdit(false);
+      await utils.cart.getAll.invalidate();
+    },
+  });
+  const { mutate: remove, isLoading: removing } = api.cart.remove.useMutation({
+    onSuccess: () => utils.cart.getAll.invalidate(),
+  });
+  const [edit, setEdit] = useState(false);
+  return (
+    <Fragment>
+      <ItemContainer className="relative gap-4 p-4">
+        <Image
+          alt="developer image"
+          src={image}
+          height={256}
+          width={256}
+          className="w-20 rounded-full"
+        />
+        <div className="flex h-full w-full flex-col">
+          <Link href={`developer/${slug}`} className="w-fit text-lg underline">
+            {name}
+          </Link>
+          <div className="flex h-full items-center justify-between">
+            {!edit && (
+              <>
+                <p className="self-start text-sm">
+                  {comment ? comment : "Click pen to add a comment"}
+                </p>
+                <button className="self-end" onClick={() => setEdit(true)}>
+                  <Icon icon="edit" className="h-6 fill-black" />
+                </button>
+              </>
+            )}
+          </div>
+        </div>
+        <button
+          className="absolute right-1 top-1"
+          disabled={removing}
+          onClick={() => remove({ developerId })}
+        >
+          <Icon icon="star" className="w-10 fill-orange" />
+        </button>
+      </ItemContainer>
+      {edit && (
+        <form
+          onSubmit={(e) =>
+            void handleSubmit(({ comment }) =>
+              updateComment({ developerId, comment }),
+            )(e)
+          }
+          className="relative flex w-full rounded-md bg-white p-2 pr-14"
+        >
+          <textarea
+            placeholder="Add a comment if you like."
+            className="h-44 w-full bg-gray outline-none"
+            {...register("comment")}
+          />
+          <button type="button" onClick={() => setEdit(false)}>
+            <Icon
+              icon="close"
+              className="absolute right-1 top-2 h-10 fill-black"
+            />
+          </button>
+          <button>
+            <Icon
+              icon="check"
+              className="absolute bottom-2 right-1 h-10 fill-black"
+            />
+          </button>
+        </form>
+      )}
+    </Fragment>
   );
 };
 
