7cfdbbd23fbd0cd421ccc134145065a70ebc35a1
Rasmus Eklund <49008491+rasmus-eklund@users.noreply.github.com>
2023-11-24 11:09:40 +0100

    Refactor: only use slug
    Co-authored-by: Allan Heremi <allanheremi@users.noreply.github.com>

---
+++ src/app/_components/LogLink.tsx
@@ -0,0 +1,39 @@
+"use client";
+
+import { api } from "@/trpc/react";
+import { useSession } from "next-auth/react";
+import Link from "next/link";
+import type { ReactNode } from "react";
+
+type Props = {
+  children: ReactNode;
+  slug: string;
+  className?: string;
+  storeScroll?: () => void;
+};
+
+const LogLink = ({ children, slug, className, storeScroll }: Props) => {
+  const { data: session } = useSession();
+  const { mutate: logClick } = api.log.logClick.useMutation({
+    onError: () => console.log("Could not log"),
+  });
+  const handleClick = () => {
+    if (storeScroll) {
+      storeScroll();
+    }
+    if (session && session.user.role === "CLIENT") {
+      logClick({ slug });
+    }
+  };
+  return (
+    <Link
+      className={className}
+      onClick={handleClick}
+      href={`/developer/${slug}`}
+    >
+      {children}
+    </Link>
+  );
+};
+
+export default LogLink;
