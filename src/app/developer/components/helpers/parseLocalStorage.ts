ad8ef33e582792f686db2148dd93c985020277b7
Rasmus Eklund <49008491+rasmus-eklund@users.noreply.github.com>
2023-11-24 11:08:05 +0100

    Refactor: Move file
    Co-authored-by: Allan Heremi <allanheremi@users.noreply.github.com>

---
+++ src/app/developer/components/helpers/parseLocalStorage.ts
@@ -0,0 +1,37 @@
+import { localStorageSchema } from "@/utils/zodSchema";
+
+type ParsedLocalStorage = {
+  next: string | null;
+  prev: string | null;
+  search: string;
+  scrollPosition: number;
+};
+
+const parseLocalStorage = (currentSlug: string): ParsedLocalStorage => {
+  const empty = { next: null, prev: null, search: "", scrollPosition: 0 };
+  const raw = localStorage.getItem("next-devs");
+  if (!raw) {
+    return empty;
+  }
+  const parsed = localStorageSchema.safeParse(JSON.parse(raw));
+  if (!parsed.success) {
+    return empty;
+  }
+  const currentSlugIndex = parsed.data.devs
+    .map(({ slug }) => slug)
+    .indexOf(currentSlug);
+  if (currentSlugIndex === -1) {
+    return empty;
+  }
+  const next = parsed.data.devs[currentSlugIndex + 1];
+  const prev = parsed.data.devs[currentSlugIndex - 1];
+  const { search, scrollPosition } = parsed.data;
+  return {
+    next: !!next ? next.slug : null,
+    prev: !!prev ? prev.slug : null,
+    search,
+    scrollPosition,
+  };
+};
+
+export default parseLocalStorage;
