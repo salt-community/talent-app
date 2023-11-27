f2ecc24d4c5aa4cd0b4a8969903e8802da4cdddf
Rasmus Eklund <49008491+rasmus-eklund@users.noreply.github.com>
2023-11-27 16:10:11 +0100

    Style: fix shimmer
    Co-authored-by: Allan Heremi <allanheremi@users.noreply.github.com>

--- src/app/developer/components/DevShimmer.tsx
+++ src/app/developer/components/DevShimmer.tsx
@@ -2,8 +2,8 @@ import { Skeleton } from "react-activity-calendar";
 
 const DevShimmer = () => {
   return (
-    <main className="Shimmerpulse flex grow flex-col items-center gap-5 bg-gradient-to-b from-orange to-pink pb-5 pt-5 md:px-5">
-      <div className="flex h-full w-full max-w-5xl items-center justify-center bg-gray pb-10 pt-10 md:rounded-lg">
+    <main className="flex grow animate-pulse flex-col items-center gap-5 pb-5 pt-5 md:px-5">
+      <div className="flex w-full max-w-5xl items-center justify-between gap-2 rounded-md border border-black/20 bg-gray p-5 pt-10 shadow-lg">
         <div className="flex w-full animate-pulse flex-col items-center gap-4">
           <div className="h-28 w-28 rounded-full bg-black/10"></div>
           <div className="h-6 w-3/5 rounded bg-black/10"></div>
@@ -16,7 +16,7 @@ const DevShimmer = () => {
         </div>
       </div>
       <section className="flex w-full grow flex-col gap-10 bg-gray px-5 pt-5 md:max-w-5xl md:rounded-md">
-        <nav className="Shimmerpulse flex justify-around">
+        <nav className="flex animate-pulse justify-around">
           {["Skills", "Bio", "Projects"].map((i) => (
             <div
               key={`${i}-button`}
@@ -26,7 +26,7 @@ const DevShimmer = () => {
             </div>
           ))}
         </nav>
-        <div className="Shimmerpulse flex flex-col gap-4">
+        <div className="flex animate-pulse flex-col gap-4">
           <div className="flex h-10 w-full animate-pulse flex-wrap gap-2 rounded-md lg:h-8">
             <div className="w-16 rounded bg-black/10 md:w-32"></div>
             <div className="w-16 rounded bg-black/10 md:w-32"></div>
