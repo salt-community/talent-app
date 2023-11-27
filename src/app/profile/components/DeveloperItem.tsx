e631f776eb8e343098214f2093472dad021f0bd7
Rasmus Eklund <49008491+rasmus-eklund@users.noreply.github.com>
2023-11-27 15:06:03 +0100

    Feat: edit and view buttons
    Co-authored-by: Allan Heremi <allanheremi@users.noreply.github.com>

--- src/app/profile/components/DeveloperItem.tsx
+++ src/app/profile/components/DeveloperItem.tsx
@@ -2,27 +2,30 @@ import type { RouterOutputs } from "@/trpc/shared";
 import Image from "next/image";
 import Link from "next/link";
 import ItemContainer from "../../_components/ItemContainer";
+import Icon from "@/app/assets/icons/Icon";
 type NullableDeveloper = RouterOutputs["developer"]["getByUser"];
 type Developer = NonNullable<NullableDeveloper>;
 type Props = {
   developer: Developer;
 };
 
-const Developer = ({ developer: { id, image, name } }: Props) => {
+const Developer = ({ developer: { id, image, name, slug } }: Props) => {
   return (
-    <ItemContainer>
-      <Link
-        className="flex h-full w-full items-center gap-2 px-5"
-        href={`/profile/developer/${id}`}
-      >
-        <Image
-          className="h-20 w-20 rounded-full"
-          src={image}
-          alt="profile picture"
-          width={256}
-          height={256}
-        />
-        <p>{name}</p>
+    <ItemContainer className="gap-4 px-4">
+      <Image
+        className="h-20 w-20 rounded-full"
+        src={image}
+        alt="profile picture"
+        width={256}
+        height={256}
+      />
+      <p className="grow">{name}</p>
+
+      <Link href={`/developer/${slug}`}>
+        <Icon icon="eye" className="h-10" />
+      </Link>
+      <Link href={`/profile/developer/${id}`}>
+        <Icon icon="edit" className="h-10" />
       </Link>
     </ItemContainer>
   );
