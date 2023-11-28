195114575d37cea2d4128eeb10450e82b86f7756
Rasmus Eklund <49008491+rasmus-eklund@users.noreply.github.com>
2023-11-28 12:26:27 +0100

    Refactor: make icon smaller to not resize header
    Co-authored-by: Allan Heremi <allanheremi@users.noreply.github.com>

--- src/app/_components/CartStatus.tsx
+++ src/app/_components/CartStatus.tsx
@@ -5,13 +5,12 @@ import Link from "next/link";
 
 const CartStatus = () => {
   const { data: cart, isSuccess } = api.cart.getAll.useQuery();
-
   return (
     <Link href={"/cart"} className="relative">
-      <Icon icon="starCart" className="h-8 fill-black lg:h-12" />
-      <div className="absolute -right-2 -top-1 flex h-4 w-4 select-none items-center justify-center rounded-full bg-orange/90 lg:h-5 lg:w-5">
-        <p className="text-sm text-black">{isSuccess && cart.length}</p>
-      </div>
+      <Icon icon="starCart" className="h-8 fill-black lg:h-10" />
+      <p className="absolute -right-2 -top-1 flex h-4 w-4 select-none items-center justify-center rounded-full bg-orange/90 text-center text-sm text-black lg:h-5 lg:w-5">
+        {isSuccess && cart.length}
+      </p>
     </Link>
   );
 };
