a82149fafcaed33593582d3e6822f3cea07e3de2
Rasmus Eklund <49008491+rasmus-eklund@users.noreply.github.com>
2023-12-01 09:14:51 +0100

    Style: comment
    Co-authored-by: Allan Heremi <allanheremi@users.noreply.github.com>

--- src/app/admin/favorited/_components/Comment.tsx
+++ src/app/admin/favorited/_components/Comment.tsx
@@ -1,4 +1,5 @@
 "use client";
+import Icon from "@/app/assets/icons/Icon";
 import React, { useState } from "react";
 
 type Props = { name: string; comment: string };
@@ -7,13 +8,19 @@ const Comment = ({ comment, name }: Props) => {
   const [open, setOpen] = useState(false);
   return (
     <div className="relative">
-      <button onClick={() => setOpen((p) => !p)} className="underline">
+      <button onClick={() => setOpen(true)} className="underline">
         {name}
       </button>
       {open && (
-        <p className="absolute right-full top-0 h-full w-full border border-orange bg-white">
-          {comment}
-        </p>
+        <div className="absolute right-0 top-0 z-10 flex rounded-md bg-orange p-2">
+          <button
+            onClick={() => setOpen(false)}
+            className="absolute right-1 top-1"
+          >
+            <Icon icon="close" className="h-8" />
+          </button>
+          <p className="h-full w-full bg-white p-2 pr-10">{comment}</p>
+        </div>
       )}
     </div>
   );
