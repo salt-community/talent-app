701125b06456bc974449508efb1c08d42175c496
abdejohan <abdejohan@gmail.com>
2024-02-22 14:20:57 +0100

    Revert "Exclude unpublished developers from meilisearch index"
    This reverts commit 70fdbf0068a6ef18695746234734ea524e12054d.

--- src/server/seedMeilisearch.ts
+++ src/server/seedMeilisearch.ts
@@ -5,13 +5,7 @@ import { db } from "./db";
 const seedMeilisearch = async () => {
   try {
     const data = await db.developer.findMany();
-    const allowedDeveloperIds: string[] = [];
-
-    const allowedDevelopers = data.filter((dev) =>
-      allowedDeveloperIds.includes(dev.id),
-    );
-
-    const devs = allowedDevelopers.map((c) => ({
+    const devs = data.map((c) => ({
       skills: c.skills,
       title: c.title,
       name: c.name,
