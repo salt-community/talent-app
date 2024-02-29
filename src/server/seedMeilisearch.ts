a1e2ee6c3665ac2d23d6aa7251f0137b2588c358
abdejohan <abdejohan@gmail.com>
2024-02-29 14:35:02 +0100

    Split meilisearch seed function in to 2 seperate seed functions

--- src/server/seedMeilisearch.ts
+++ src/server/seedMeilisearch.ts
@@ -3,16 +3,50 @@ import settings from "./meilisearchSettings";
 import { db } from "./db";
 import { allowedDeveloperIds } from "@/allowed-developer-ids";
 
-const seedMeilisearch = async () => {
+const seedMeilisearchWithNonPayRollDevs = async () => {
+  try {
+    const data = await db.developer.findMany();
+
+    const notAllowedDevelopers = data.filter(
+      (dev) => !allowedDeveloperIds.includes(dev.id),
+    );
+
+    const nonPayrollDevs = notAllowedDevelopers.map((c) => ({
+      skills: c.skills,
+      title: c.title,
+      name: c.name,
+      description: c.description,
+      slug: c.slug,
+      image: c.image,
+      locations: c.locationPref,
+      id: c.id,
+    }));
+
+    await client.deleteIndex("non-payrolled-developers");
+    await client.index("non-payrolled-developers").addDocuments(nonPayrollDevs);
+    await client
+      .index("non-payrolled-developers")
+      .updateSearchableAttributes([
+        "skills",
+        "locations",
+        "title",
+        "description",
+        "name",
+      ]);
+
+    await client.index("non-payrolled-developers").updateSettings(settings);
+    await client.index("non-payrolled-developers").getSettings();
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+const seedMeilisearchWithPayRollDevs = async () => {
   try {
     const data = await db.developer.findMany();
 
     const allowedDevelopers = data.filter((dev) =>
       allowedDeveloperIds.includes(dev.id),
-    );
-
-    const notAllowedDevelopers = data.filter(
-      (dev) => !allowedDeveloperIds.includes(dev.id),
     );
 
     const devs = allowedDevelopers.map((c) => ({
@@ -26,23 +60,8 @@ const seedMeilisearch = async () => {
       id: c.id,
     }));
 
-    const nonPayrollDevs = notAllowedDevelopers.map((c) => ({
-      skills: c.skills,
-      title: c.title,
-      name: c.name,
-      description: c.description,
-      slug: c.slug,
-      image: c.image,
-      locations: c.locationPref,
-      id: c.id,
-    }));
-
     await client.deleteIndex("developers");
-    await client.deleteIndex("non-payrolled-developers");
-
     await client.index("developers").addDocuments(devs);
-    await client.index("non-payrolled-developers").addDocuments(nonPayrollDevs);
-
     await client
       .index("developers")
       .updateSearchableAttributes([
@@ -54,11 +73,18 @@ const seedMeilisearch = async () => {
       ]);
 
     await client.index("developers").updateSettings(settings);
-    const updatedSettings = await client.index("developers").getSettings();
+    await client.index("developers").getSettings();
+  } catch (error) {
+    console.error(error);
+  }
+};
 
-    console.log(updatedSettings);
-  } catch (err: unknown) {
-    console.log(err);
+const seedMeilisearch = async () => {
+  try {
+    await seedMeilisearchWithNonPayRollDevs();
+    await seedMeilisearchWithPayRollDevs();
+  } catch (error) {
+    console.error(error);
   }
 };
 
