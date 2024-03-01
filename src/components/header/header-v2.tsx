41e9301c0adbdba4f8a8124f5a40427feba514ee
Marcus Rådell <marcus@radell.net>
2024-03-01 08:55:41 +0100

    add brand to header v2

--- src/components/header/header-v2.tsx
+++ src/components/header/header-v2.tsx
@@ -1,9 +1,26 @@
-import { Navbar, NavbarContent } from "@nextui-org/react";
+import { NavbarBrand, NavbarContent } from "@nextui-org/react";
+import Image from "next/image";
+import Link from "next/link";
 import { Search } from "./search";
+import { Navbar } from "./navbar";
 
 export const HeaderV2 = () => {
   return (
-    <Navbar className="bg-gradient-to-r from-[rgb(255,121,97)] to-[rgb(243,92,126)]">
+    <Navbar>
+      <NavbarContent>
+        <NavbarBrand>
+          <Link href={"/"}>
+            <Image
+              className="w-24"
+              src="/logoBlack.png"
+              alt="salt logo"
+              width={130}
+              height={30}
+              data-cy="logo"
+            />
+          </Link>
+        </NavbarBrand>
+      </NavbarContent>
       <NavbarContent>
         <Search />
       </NavbarContent>
