2b4368450247a2790708cdf6cfa57467e7899a48
Marcus Rådell <marcus@radell.net>
2024-03-01 21:33:57 +0100

    update allowedDevelopers

--- src/allowed-developer-ids.ts
+++ src/allowed-developer-ids.ts
@@ -1,12 +1,8 @@
 const allowedDevelopers = [
-  { name: "Marisa Pinheiro", id: "cloe61g35000jjw08ral2bhl7" },
   { name: "Anne Mariel Catapang", id: "clozmeimd0003k308g1zn9yxn" },
   { name: "Svitlana Rybakova", id: "clq5lfr720000ic08tvcch7t5" },
   { name: "Adila Razmi", id: "clr7tyrai0002l108bfuy3rwn" },
-  { name: "Maidelin Rubio", id: "clr7tj85d0000l108epi1l5ow" },
   { name: "Davit Danielyan", id: "clr7waefp0000jt08ira5x64g" },
-  { name: "Chris Stephens", id: "clr7v5fnn0000l90835s7cdsq" },
-  { name: "Mumtaz Fatima", id: "clswwx2ao0000lg08npshl26y" },
   { name: "Ayoub Zannachi", id: "clrp2kirq0007js08b50y5jra" },
   { name: "Kevin Gida", id: "clozvcfya0006l508qgegwx2p" },
   { name: "Silvia A. Dominguez R.", id: "clsd0usa30000l708udg7jxam" },
