590078b67447f543fd0baf9543bc1a12c98b9856
Marcus Rådell <marcus@radell.net>
2024-03-01 10:49:33 +0100

    add navbar menu stub

--- src/components/header/menu.tsx
+++ src/components/header/menu.tsx
@@ -1,5 +1,12 @@
-import { NavbarMenuToggle } from "@nextui-org/react";
+import { Link, NavbarMenu, NavbarMenuToggle } from "@nextui-org/react";
 
 export const Menu = () => {
-  return <NavbarMenuToggle />;
+  return (
+    <>
+      <NavbarMenuToggle />
+      <NavbarMenu className="z-100 flex flex-col items-center gap-4 pt-10">
+        <Link href="#">Sign Out</Link>
+      </NavbarMenu>
+    </>
+  );
 };
