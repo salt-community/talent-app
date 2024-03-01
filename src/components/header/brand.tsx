195d71297e09f1ccb7fbd09d54eb69b0a8c60f39
Marcus Rådell <marcus@radell.net>
2024-03-01 09:47:42 +0100

    adjust brand

--- src/components/header/brand.tsx
+++ src/components/header/brand.tsx
@@ -1,20 +1,12 @@
-import { NavbarBrand, NavbarContent } from "@nextui-org/react";
+import { NavbarBrand, NavbarContent, Image } from "@nextui-org/react";
 import Link from "next/link";
-import Image from "next/image";
 
 export const Brand = () => {
   return (
     <NavbarContent>
       <NavbarBrand>
         <Link href={"/"}>
-          <Image
-            className="w-24"
-            src="/logoBlack.png"
-            alt="salt logo"
-            width={130}
-            height={30}
-            data-cy="logo"
-          />
+          <Image src="/logoBlack.png" alt="salt logo" width={96} />
         </Link>
       </NavbarBrand>
     </NavbarContent>
