469eee09e841381dfa9c675242163a04609ebb97
Marcus Rådell <marcus@radell.net>
2024-03-01 10:49:21 +0100

    improve search query string

--- src/components/header/search.tsx
+++ src/components/header/search.tsx
@@ -4,6 +4,13 @@ import { useRouter } from "next/navigation";
 import { useState } from "react";
 import { SearchIcon } from "./search-icon";
 
+const route = (search: string) => {
+  if (!search) {
+    return "/";
+  }
+  return `/?search=${encodeURIComponent(search)}`;
+};
+
 export const Search = () => {
   const router = useRouter();
   const [search, setSearch] = useState("");
@@ -18,7 +25,7 @@ export const Search = () => {
         onKeyUp={(e) => {
           if (e.key === "Enter") {
             e.preventDefault();
-            router.push(`/?search=${encodeURIComponent(search)}`);
+            router.push(route(search));
           }
         }}
         startContent={<SearchIcon />}
