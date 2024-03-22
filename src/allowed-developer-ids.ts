63fdd2fa3091bef8698401630a9647c336b7fb35
Marcus Rådell <marcus@radell.net>
2024-03-22 14:49:48 +0100

    update allowed developers list

--- src/allowed-developer-ids.ts
+++ src/allowed-developer-ids.ts
@@ -2,10 +2,16 @@ const allowedDevelopers = [
   { name: "Anne Mariel Catapang", id: "clozmeimd0003k308g1zn9yxn" },
   { name: "Svitlana Rybakova", id: "clq5lfr720000ic08tvcch7t5" },
   { name: "Adila Razmi", id: "clr7tyrai0002l108bfuy3rwn" },
-  { name: "Davit Danielyan", id: "clr7waefp0000jt08ira5x64g" },
   { name: "Ayoub Zannachi", id: "clrp2kirq0007js08b50y5jra" },
   { name: "Kevin Gida", id: "clozvcfya0006l508qgegwx2p" },
   { name: "Silvia A. Dominguez R.", id: "clsd0usa30000l708udg7jxam" },
+  { id: "clt718sc3000ujv08ssxldmmu", name: "Jacob Heinegård" },
+  { id: "cltcvioo40000l808mhemplfl", name: "Jens Ortner" },
+  { id: "clt8dvm9i0000jv08wex19mje", name: "Mauro Morales" },
+  { id: "clsycnzbc0000l608rucsofkt", name: "Hampus Söderberg" },
+  { id: "clt70i6xd000ajv08w92faydx", name: "Kostadinka Karova" },
+  { id: "clu2pr1pc000bjg08hdt9mhjy", name: "Jagoda Bodnar" },
+  { id: "clt70ginq0005l908qq52oucr", name: "Anthony Raynaud" },
 ];
 
 export const allowedDeveloperIds: string[] = allowedDevelopers.map(
