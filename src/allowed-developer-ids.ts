27baf1c4c92396c904e0694499215a2884582512
alexmiloeski <a.miloeski@gmail.com>
2024-03-27 10:22:48 +0100

    remove adila from allowed list

--- src/allowed-developer-ids.ts
+++ src/allowed-developer-ids.ts
@@ -1,7 +1,6 @@
 const allowedDevelopers = [
   { name: "Anne Mariel Catapang", id: "clozmeimd0003k308g1zn9yxn" },
   { name: "Svitlana Rybakova", id: "clq5lfr720000ic08tvcch7t5" },
-  { name: "Adila Razmi", id: "clr7tyrai0002l108bfuy3rwn" },
   { name: "Ayoub Zannachi", id: "clrp2kirq0007js08b50y5jra" },
   { name: "Kevin Gida", id: "clozvcfya0006l508qgegwx2p" },
   { name: "Silvia A. Dominguez R.", id: "clsd0usa30000l708udg7jxam" },
